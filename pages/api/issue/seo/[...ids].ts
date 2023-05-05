b07a62fbdcb5a09efc35e9c194529ae233062dfb
Vitor Hugo <vhcsilvaa@gmail.com>
2023-05-05 13:22:13 -0300

    removing put method

--- pages/api/issue/seo/[...ids].ts
+++ pages/api/issue/seo/[...ids].ts
@@ -1,19 +1,9 @@
 import {NextApiRequest, NextApiResponse} from "next";
-import getConfig from "next/config";
-import { Octokit } from "octokit";
 import {Op, Sequelize} from "sequelize";
 
 import models from "db/models";
 
-import * as IssueQueries from "graphql/issue";
-
-import { getPropertyRecursively } from "helpers/object";
-
 import { IssueRoute } from "middleware/issue-route";
-
-import { GraphQlQueryResponseData, GraphQlResponse } from "types/octokit";
-
-const { serverRuntimeConfig } = getConfig();
 
 async function get(req: NextApiRequest, res: NextApiResponse) {
   const { ids: [repoId, ghId, networkName, chainName] } = req.query;
@@ -53,75 +43,10 @@ async function get(req: NextApiRequest, res: NextApiResponse) {
   return res.status(200).json(issue);
 }
 
-async function put(req: NextApiRequest, res: NextApiResponse) {
-  const {
-    ids: [repoId, ghId, networkName],
-  } = req.query;
-
-  const {
-    body,
-    tags
-  } = req.body;
-
-  const issueId = [repoId, ghId].join("/");
-
-  const network = await models.network.findOne({
-    where: {
-      name: {
-        [Op.iLike]: String(networkName).replaceAll(" ", "-")
-      }
-    }
-  });
-
-  if (!network) return res.status(404).json({message:"Invalid network"});
-
-  const issue = await models.issue.findOne({
-    where: {
-      issueId,
-      network_id: network?.id
-    },
-    include: [{ association: "repository" }]
-  });
-
-  if (!issue) return res.status(404).json({message: "bounty not found"});
-
-  if(issue.state === 'draft'){
-    if(body) issue.body = body
-    if(tags) issue.tags = tags
-    await issue.save()
-
-    const [owner, repo] = issue.repository.githubPath.split("/");
-
-    const githubAPI = (new Octokit({ auth: serverRuntimeConfig?.github?.token })).graphql;
-
-    const issueDetails = await githubAPI<GraphQlResponse>(IssueQueries.Details, {
-      repo,
-      owner,
-      issueId: +issue.githubId
-    });
-  
-    const issueGithubId = issueDetails.repository.issue.id;
-    
-    const updateIssue = getPropertyRecursively<GraphQlQueryResponseData>("node",
-                                                                         await githubAPI(IssueQueries.Update, {
-                                                                            body: body,
-                                                                            issueId: issueGithubId,
-                                                                         }));
-
-    return res.status(200).json(updateIssue);
-  } else{
-    return res.status(400).json({message: 'bounty not in draft'})
-  } 
-}
-
-async function IssuesMethods(req: NextApiRequest,
-                             res: NextApiResponse) {
+async function IssueSEO(req: NextApiRequest, res: NextApiResponse) {
   switch (req.method.toLowerCase()) {
   case "get":
     await get(req, res);
-    break;
-  case "put":
-    await put(req, res);
     break;
 
   default:
@@ -131,4 +56,4 @@ async function IssuesMethods(req: NextApiRequest,
   res.end();
 }
 
-export default IssueRoute(IssuesMethods)
+export default IssueRoute(IssueSEO);
