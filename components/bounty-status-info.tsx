647e801976d40a9a3fa0bfae5ade8dd5ee42a52e
Marcus Vinícius <marcusvinici25@gmail.com>
2023-05-10 16:51:55 -0300

    adjusting logic to partial-funded

--- components/bounty-status-info.tsx
+++ components/bounty-status-info.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import BigNumber from "bignumber.js";
+
 import BountyDoneIcon from "assets/icons/bounty-done-icon";
 import CanceledIcon from "assets/icons/canceled-icon";
 import CircleIcon from "assets/icons/circle-icon";
@@ -8,12 +10,14 @@ import { IssueState } from "interfaces/issue-data";
 
 interface IBountyStatusInfo {
   issueState: IssueState;
+  fundedAmount?: BigNumber;
 }
 
-export default function BountyStatusInfo({ issueState }: IBountyStatusInfo) {
+export default function BountyStatusInfo({ issueState, fundedAmount }: IBountyStatusInfo) {
+  const state = fundedAmount?.gt(0) ? 'partial-funded' : issueState
   if (issueState === "closed") return <BountyDoneIcon />;
 
   if (issueState === "canceled") return <CanceledIcon />;
 
-  return <CircleIcon type={issueState} />;
+  return <CircleIcon type={state} />;
 }
