4d2feb42509167af89dbf61d3fd91bb7d53c6db5
Marcus Vinícius <marcusvinici25@gmail.com>
2023-05-17 16:43:56 -0300

    fix SSL error localhost

--- db/config/index.js
+++ db/config/index.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 
+const dbHost = process.env.NEXT_DB_HOST;
+
 module.exports = {
   dialect: process.env.NEXT_DB_DIALECT || "postgres",
   username: process.env.NEXT_DB_USERNAME || "github",
@@ -14,7 +16,7 @@ module.exports = {
     logging: false
   }),
 
-  ...(process.env.NEXT_DB_HOST
+  ...(dbHost && dbHost !== "localhost"
     ? {
       dialectOptions: {
 
