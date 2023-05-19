0bedc5096e6f191a1b9f586513fa16b1f559838c
Marcus Vinícius <marcusvinici25@gmail.com>
2023-05-19 15:21:57 -0300

    adding logic tablet support in item-row

--- components/bounty/tabs-sections/item-row.tsx
+++ components/bounty/tabs-sections/item-row.tsx
@@ -9,6 +9,8 @@ import PullRequestLabels, { IPRLabel } from "components/pull-request-labels";
 
 import { truncateAddress } from "helpers/truncate-address";
 
+import ReviewsNumber from "./reviews-number";
+
 interface ItemRowProps {
   id: string | number;
   githubLogin?: string;
@@ -16,24 +18,34 @@ interface ItemRowProps {
   status?: IPRLabel[];
   children?: React.ReactNode;
   href?: UrlObject | string;
+  reviewers?: number;
+  isProposal?: boolean;
 }
 
-function ItemRow({ id, githubLogin, creator, status, children, href }: ItemRowProps) {
+function ItemRow({ id, githubLogin, creator, status, children, href, reviewers, isProposal }: ItemRowProps) {
+
+  function RenderId({ className }: { className?: string}) {
+    return (
+      <div className={className}>
+        <span className="label-m text-gray-500">#{id}</span>
+      </div>
+    );
+  }
+
+
   return (
     <Link passHref key={`${githubLogin}-${id}`} href={href || "#"}>
       <div
-        className={`d-flex flex-row p-20 border-radius-8 bg-gray-850 align-items-center ${
+        className={`row d-flex flex-row py-3 px-2 border-radius-8 bg-gray-900 align-items-center ${
           href ? "cursor-pointer" : ""
         }`}
       >
         <div className="col d-flex flex-row align-items-center gap-3">
-          <div className="col-1">
-            <span className="label-m text-gray-500">#{id}</span>
-          </div>
+          <RenderId className="col-1 d-none d-xl-block" />
           <div className="col-md-5 col-xl-4 d-flex align-items-center gap-2">
           {githubLogin ? (
               <>
-                <Avatar userLogin={githubLogin} />
+                <Avatar userLogin={githubLogin} size="sm"/>
                 <span className={`text-uppercase text-white caption ${status?.length && 'text-truncate'}`}>
                   {githubLogin}
                 </span>
@@ -47,14 +59,16 @@ function ItemRow({ id, githubLogin, creator, status, children, href }: ItemRowPr
               </>
             )}
           </div>
+          {!isProposal && <ReviewsNumber reviewers={reviewers} className="d-xl-none"/>}
           <div className="col-4 d-flex gap-2">
             {status?.length
               ? status.map((st) => <PullRequestLabels {...st} />)
               : null}
           </div>
         </div>
-        <div className="col d-flex flex-row gap-3 justify-content-end">
+        <div className="col d-flex flex-row gap-3 justify-content-end align-items-center">
           {children}
+          <RenderId className="d-xl-none" />
         </div>
       </div>
     </Link>
