03f905dc8244da06d62203dade728191fe22ca32
Vitor Hugo <vhcsilvaa@gmail.com>
2023-05-30 10:05:12 -0300

    adding user agent check

--- x-hooks/use-breakpoint.ts
+++ x-hooks/use-breakpoint.ts
@@ -14,18 +14,39 @@ const getCurrentBreakPoint = (currentWidth: number) => {
   return "xxl";
 }
 
-export default function useBreakPoint() {
+const isMobileUserAgent = () => {
+  if (!window) return false;
+
+  const userAgentMatch = agent => !!navigator.userAgent.match(agent);
+
+  if (userAgentMatch(/Android/i) ||
+      userAgentMatch(/webOS/i) ||
+      userAgentMatch(/iPhone/i) ||
+      userAgentMatch(/iPad/i) ||
+      userAgentMatch(/iPod/i) ||
+      userAgentMatch(/BlackBerry/i) ||
+      userAgentMatch(/Windows Phone/i))
+    return true;
+
+  return false;
+}
+
+export default function useBreakPoint(validateAgent = false) {
   const [currentBreakPoint, setCurrentBreakpoint] = useState<string>();
+  const [userAgentCheck, setUserAgentCheck] = useState(false);
 
   const handler = () => setCurrentBreakpoint(getCurrentBreakPoint(window.innerWidth));
   const debouncedHandler = useDebouncedCallback(handler, 300);
 
-  const isMobileView = ["xs", "sm", "md"].includes(currentBreakPoint);
-  const isTabletView = currentBreakPoint === "lg";
-  const isDesktopView = ["xl", "xxl"].includes(currentBreakPoint);
+  const isMobileView = ["xs", "sm", "md"].includes(currentBreakPoint) && userAgentCheck;
+  const isTabletView = currentBreakPoint === "lg" && userAgentCheck;
+  const isDesktopView = ["xl", "xxl"].includes(currentBreakPoint) && userAgentCheck;
 
   useEffect(() => {
+    setUserAgentCheck(validateAgent ? isMobileUserAgent() : true);
+
     handler();
+
     const observer = new ResizeObserver(debouncedHandler);
     
     observer.observe(document.documentElement);
