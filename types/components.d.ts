243afe24ab35210e116cdeb8530bc0a063f43f1e
Vitor Hugo <vhcsilvaa@gmail.com>
2023-06-13 10:00:31 -0300

    creating new component for shared list item

--- types/components.d.ts
+++ types/components.d.ts
@@ -1,7 +1,15 @@
 import { IssueBigNumberData } from "interfaces/issue-data";
 
 import { SearchBountiesPaginated } from "types/api";
+import { BreakpointOptions } from "types/utils";
 
 export interface SearchBountiesPaginatedBigNumber extends Omit<SearchBountiesPaginated, "rows"> {
   rows: IssueBigNumberData[];
+}
+
+export interface ResponsiveListItemColumnProps {
+  label: string;
+  secondaryLabel?: string;
+  breakpoints?: BreakpointOptions;
+  currency?: string;
 }
