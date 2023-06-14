a1938cf192c4b38da2180009160d9b0f853c2d1a
Vitor Hugo <vhcsilvaa@gmail.com>
2023-06-14 14:07:21 -0300

    refactoring page

--- pages/leaderboard.tsx
+++ pages/leaderboard.tsx
@@ -1,33 +1,13 @@
 import { GetServerSideProps } from "next";
-import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
-import LeaderBoardList  from "components/leaderboard-list/controller";
-import PageHero from "components/page-hero";
+import LeaderBoardPage from "components/pages/leaderboard/view";
 
 import { emptyPaginatedData } from "helpers/api";
 
-import { LeaderBoardPageProps } from "types/pages";
-
 import getLeaderboardData from "x-hooks/api/get-leaderboard-data";
 
-export default function LeaderBoardPage({ leaderboard }: LeaderBoardPageProps) {
-  const { t } = useTranslation(["common", "leaderboard"]);
-
-  return (
-    <>
-      <PageHero
-        title={t("leaderboard:title")}
-        subtitle={t("leaderboard:sub-title")}
-        infos={[]}
-      />
-
-      <LeaderBoardList
-        leaderboard={leaderboard}
-      />
-    </>
-  );
-}
+export default LeaderBoardPage;
 
 export const getServerSideProps: GetServerSideProps = async ({ query, locale }) => {
   const leaderboard = await getLeaderboardData(query)
