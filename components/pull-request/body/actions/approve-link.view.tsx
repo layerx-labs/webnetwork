804e607266ad105e6db7ea6706e7d1a7b94a1697
Marcus Vinícius <marcusvinici25@gmail.com>
2023-06-16 16:14:55 -0300

    adding className to approve-link component

--- components/pull-request/body/actions/approve-link.view.tsx
+++ components/pull-request/body/actions/approve-link.view.tsx
@@ -6,15 +6,18 @@ export default function ApproveLink({
   hrefPath,
   forcePath,
   color = "primary",
+  className,
 }: {
   hrefPath?: string;
   forcePath?: string;
   color?: string;
+  className?: string;
 }) {
   const { t } = useTranslation(["common"]);
 
   return (
     <GithubLink
+      className={className}
       forcePath={hrefPath}
       hrefPath={forcePath}
       color={color}
