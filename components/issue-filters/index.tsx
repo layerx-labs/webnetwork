689f01529a69d0659d78c33a5b508d3c29118063
Marcus Vinícius <marcusvinici25@gmail.com>
2023-06-21 13:30:33 -0300

    adding isProfile logic to IssueFilters

--- components/issue-filters/index.tsx
+++ components/issue-filters/index.tsx
@@ -1,15 +1,20 @@
 import IssueDesktopFilters from "components/issue-filters/desktop-filters";
 import IssueMobileFilters from "components/issue-filters/mobile-filters";
 
-export default function IssueFilters({ onlyTimeFrame = false }) {
+export default function IssueFilters({
+  onlyTimeFrame = false,
+  isProfile = false,
+}) {
   return (
     <>
-      <div className="d-none d-xl-flex">
-        <IssueDesktopFilters onlyTimeFrame={onlyTimeFrame} />
-      </div>
+      {!isProfile && (
+        <div className="d-none d-xl-flex">
+          <IssueDesktopFilters onlyTimeFrame={onlyTimeFrame} />
+        </div>
+      )}
 
       <div className="d-flex d-xl-none">
-        <IssueMobileFilters onlyTimeFrame={onlyTimeFrame} />
+        <IssueMobileFilters onlyTimeFrame={onlyTimeFrame} isProfile={isProfile} />
       </div>
     </>
   );
