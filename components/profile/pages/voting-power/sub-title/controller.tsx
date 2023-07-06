72bb44a15228c04608cebc1c898276279c29070b
Marcus Vinícius <marcusvinici25@gmail.com>
2023-07-06 15:34:10 -0300

    creating VotingPowerSubTitle component

---
+++ components/profile/pages/voting-power/sub-title/controller.tsx
@@ -0,0 +1,62 @@
+import clsx from "clsx";
+
+import TotalVotesTitleView from "./view";
+
+export default function VotingPowerSubTitle({
+  label,
+  infoTooltip,
+  total,
+  votesSymbol,
+  variant,
+  tokenColor,
+}: {
+  label: string;
+  infoTooltip: string;
+  total: string;
+  votesSymbol: string;
+  variant: "network" | "multi-network";
+  tokenColor: string;
+}) {
+  const getTitleSpanClass = (type: string) =>
+    `family-Regular text-white font-weight-500 ${type}`;
+  const getAmountClass = (type: string) =>
+    clsx([
+      `d-flex flex-row justify-content-center align-items-center gap-2 ${type}`,
+      "text-white py-2 px-3 border-radius-4 border border-gray-800 font-weight-medium",
+      variant === "network" ? "bg-gray-900" : "bg-gray-950",
+    ]);
+  const responsiveMobile = { sm: true, xs: true, md: false, xxl: false };
+  const responsiveDesktopAndTablet = {
+    sm: false,
+    xs: false,
+    md: true,
+    xxl: true,
+  };
+
+  function getTextColorProps() {
+    if (tokenColor)
+      return {
+        style: {
+          color: tokenColor,
+        },
+      };
+
+    return {
+      className: "text-primary",
+    };
+  }
+
+  return (
+    <TotalVotesTitleView
+      label={label}
+      infoTooltip={infoTooltip}
+      total={total}
+      votesSymbol={votesSymbol}
+      propsMobile={responsiveMobile}
+      propsDesktopAndTablet={responsiveDesktopAndTablet}
+      getTextColorProps={getTextColorProps}
+      getTitleSpanClass={getTitleSpanClass}
+      getAmountClass={getAmountClass}
+    />
+  );
+}
---
+++ components/profile/pages/voting-power/sub-title/view.tsx
@@ -0,0 +1,81 @@
+
+import TokenSymbolView from "components/common/token-symbol/view";
+import InfoTooltip from "components/info-tooltip";
+import ResponsiveWrapper from "components/responsive-wrapper";
+
+import { formatStringToCurrency } from "helpers/formatNumber";
+
+interface ResponsiveProps {
+  sm: boolean;
+  xs: boolean;
+  md: boolean;
+  xxl: boolean;
+}
+
+export default function VotingPowerSubTitleView({
+  label,
+  total,
+  votesSymbol,
+  propsMobile,
+  propsDesktopAndTablet,
+  infoTooltip,
+  getTextColorProps,
+  getTitleSpanClass,
+  getAmountClass,
+}: {
+  label: string;
+  infoTooltip: string;
+  total: string;
+  votesSymbol: string;
+  propsMobile: ResponsiveProps;
+  propsDesktopAndTablet: ResponsiveProps;
+  getTextColorProps: () => { className: string } | { style: { color: string } };
+  getTitleSpanClass: (type: string) => string;
+  getAmountClass: (type: string) => string;
+}) {
+
+  function handleAmount(icon = true) {
+    return (
+      <>
+        <span>
+          {formatStringToCurrency(total)}
+        </span>
+
+        <TokenSymbolView name={votesSymbol} {...getTextColorProps()} />
+        {icon && (
+          <InfoTooltip
+            description={infoTooltip}
+            secondaryIcon
+          />
+        )}
+      </>
+    );
+  }
+
+  return (
+    <>
+      <ResponsiveWrapper {...propsMobile}>
+        <span className={getTitleSpanClass("fs-smallest")}>
+          {label}
+        </span>
+      </ResponsiveWrapper>
+      <ResponsiveWrapper {...propsDesktopAndTablet}>
+        <span className={getTitleSpanClass("h4")}>
+          {label}
+        </span>
+      </ResponsiveWrapper>
+      <ResponsiveWrapper
+        {...propsMobile}
+        className={getAmountClass("fs-smallest")}
+      >
+        {handleAmount(false)}
+      </ResponsiveWrapper>
+      <ResponsiveWrapper
+        {...propsDesktopAndTablet}
+        className={getAmountClass("h4")}
+      >
+        {handleAmount()}
+      </ResponsiveWrapper>
+    </>
+  );
+}
