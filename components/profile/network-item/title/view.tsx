08f2947a7cc4fc0c3fe294c813a340e2448d32ef
Marcus Vinícius <marcusvinici25@gmail.com>
2023-07-07 12:29:06 -0300

    adding xs-medium to title networkName

--- components/profile/network-item/title/view.tsx
+++ components/profile/network-item/title/view.tsx
@@ -34,7 +34,7 @@ export default function NetworkItemTitleView({
         )}
       </FlexColumn>
       <FlexColumn className="justify-content-center">
-        <FlexRow className="flex-wrap">{networkName}</FlexRow>
+        <FlexRow className="flex-wrap xs-medium">{networkName}</FlexRow>
 
         {subNetworkText && (
           <FlexRow className="d-none d-sm-block">
