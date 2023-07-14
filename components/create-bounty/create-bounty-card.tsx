03357532807338660437736ffaf75ea9cde5b4d3
Marcus Vinícius <marcusvinici25@gmail.com>
2023-07-14 14:46:55 -0300

    adjusting CreateBountyCard logic

--- components/create-bounty/create-bounty-card.tsx
+++ components/create-bounty/create-bounty-card.tsx
@@ -12,30 +12,24 @@ export default function CreateBountyCard({
   maxSteps,
   children,
 }: CardProps) {
-  
-  function Card({ children }) {
-    return (
-      <>
-        <ResponsiveWrapper className="mx-3 flex-column" xs={true} md={false}>
-          {children}
-        </ResponsiveWrapper>
-        <ResponsiveWrapper
-          className="mx-2 flex-column bg-gray-900 p-4 border-radius-4 border border-gray-850"
-          xs={false}
-          md={true}
-        >
-          {children}
-        </ResponsiveWrapper>
-      </>
-    );
-  }
-
   return (
-    <Card>
-      <span className="text-gray">
-        Step {currentStep} of {maxSteps}
-      </span>
-      {children}
-    </Card>
+    <>
+      <ResponsiveWrapper className="mx-3 flex-column" xs={true} md={false}>
+        <span className="text-gray">
+          Step {currentStep} of {maxSteps}
+        </span>
+        {children}
+      </ResponsiveWrapper>
+      <ResponsiveWrapper
+        className="mx-2 flex-column bg-gray-900 p-4 border-radius-4 border border-gray-850"
+        xs={false}
+        md={true}
+      >
+        <span className="text-gray">
+          Step {currentStep} of {maxSteps}
+        </span>
+        {children}
+      </ResponsiveWrapper>
+    </>
   );
 }
