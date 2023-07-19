a6f241b489cbf3be964863c7994175eb7701fb94
Marcus Vinícius <marcusvinici25@gmail.com>
2023-07-19 15:42:37 -0300

    adding serviceFeeModalRow

--- components/create-bounty/token-amount/service-fees-modal/view.tsx
+++ components/create-bounty/token-amount/service-fees-modal/view.tsx
@@ -6,6 +6,8 @@ import { DAPPKIT_LINK } from "helpers/constants";
 
 import { DistributedAmounts } from "interfaces/proposal";
 
+import ServiceFeesModalRow from "./row/view";
+
 interface ServiceFeesModalProps {
   show: boolean;
   onClose: () => void;
@@ -21,24 +23,6 @@ export default function ServiceFeesModalView({
 }: ServiceFeesModalProps) {
   const { t } = useTranslation(["common", "bounty"]);
 
-  function ItemRow({ label, percentage, amount, symbol, border = true }) {
-    return (
-      <div
-        key={label}
-        className={`d-flex py-3 flex-wrap justify-content-between ${
-          border ? "border-bottom border-gray-700" : ""
-        }`}
-      >
-        <span className="text-gray">
-          {label} <span className="text-white">{percentage || "0"}%</span>
-        </span>
-        <span className="text-uppercase">
-          {amount || 0} <span className="text-gray">{symbol || "token"}</span>
-        </span>
-      </div>
-    );
-  }
-
   return (
     <Modal show={show} title={"Service Fees"} onCloseClick={onClose}>
       <div className="px-3">
@@ -48,19 +32,19 @@ export default function ServiceFeesModalView({
             Documentation
           </a>
         </span>
-        <ItemRow
+        <ServiceFeesModalRow
           label={t("bounty:proposal-merger")}
           percentage={distributions?.mergerAmount?.percentage}
           amount={distributions?.mergerAmount?.value}
           symbol={symbol}
         />
-        <ItemRow
+        <ServiceFeesModalRow
           label={t("bounty:network-fee")}
           percentage={distributions?.treasuryAmount?.percentage}
           amount={distributions?.treasuryAmount?.value}
           symbol={symbol}
         />
-        <ItemRow
+        <ServiceFeesModalRow
           label={t("bounty:proposal-creator")}
           percentage={distributions?.proposerAmount?.percentage}
           amount={distributions?.proposerAmount?.value}
