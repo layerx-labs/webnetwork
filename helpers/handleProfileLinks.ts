7065ad83f06b061f17509d224434be824d64ab81
Marcus Vinícius <marcusvinici25@gmail.com>
2023-07-21 16:14:33 -0300

    removing handleProfileLinks

--- helpers/handleProfileLinks.ts
+++
@@ -1,6 +0,0 @@
-import { LinkProps } from "types/components";
-
-export default function handleLinksWithoutNetwork(links: LinkProps[],
-                                                  isOnNetwork: boolean) {
-  return links.slice(0, isOnNetwork ? links.length : -1);
-}
