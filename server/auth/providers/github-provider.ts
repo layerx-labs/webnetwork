e91c15b580f5ad3de7066c9cc762a8b6b92db00f
Vitor Hugo <vhcsilvaa@gmail.com>
2023-08-04 17:53:48 -0300

    handle providers callbacks

--- server/auth/providers/github-provider.ts
+++ server/auth/providers/github-provider.ts
@@ -1,6 +1,9 @@
-import { Provider } from "next-auth/providers";
+
+import { JWT } from "next-auth/jwt";
 import GithubProvider from "next-auth/providers/github";
 import getConfig from "next/config";
+
+import { AuthProvider } from "server/auth/providers";
 
 const { serverRuntimeConfig } = getConfig();
 
@@ -13,18 +16,44 @@ interface Profile {
   avatar_url: string;
 }
 
-export const GHProvider: Provider = GithubProvider({
-  clientId: serverRuntimeConfig?.github?.clientId,
-  clientSecret: serverRuntimeConfig?.github?.secret,
-  authorization:
-    "https://github.com/login/oauth/authorize?scope=read:user+user:email+repo",
-  profile(profile: Profile) {
-    return {
-      id: profile.id,
-      name: profile.name,
-      login: profile.login,
-      email: profile.email,
-      image: profile.avatar_url,
-    };
-  },
+export const GHProvider= (currentToken: JWT): AuthProvider => ({
+  config: GithubProvider({
+    clientId: serverRuntimeConfig?.github?.clientId,
+    clientSecret: serverRuntimeConfig?.github?.secret,
+    authorization:
+      "https://github.com/login/oauth/authorize?scope=read:user+user:email+repo",
+    profile(profile: Profile) {
+      return {
+        id: profile.id,
+        name: profile.name,
+        login: profile.login,
+        email: profile.email,
+        image: profile.avatar_url,
+      };
+    },
+  }),
+  callbacks: {
+    async signIn({ profile }) {
+      if (!profile?.login) return "/?authError=Profile not found";
+
+      return true;
+    },
+    async jwt({ profile, account, token }) {
+      const { name, login } = profile;
+      const { provider, access_token } = account;
+
+      return {
+        ...currentToken,
+        ...token, 
+        profile: {
+          name,
+          login,
+        },
+        account: {
+          provider,
+          access_token
+        }
+      };
+    },
+  }
 });
