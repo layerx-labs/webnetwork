aece1f2e8685d4cb79b7caa06a60b2ade374752d
Vitor Hugo <vhcsilvaa@gmail.com>
2023-08-07 16:39:03 -0300

    move to correct folder

--- interfaces/validators/address.ts
+++
@@ -1,9 +0,0 @@
-import getConfig from "next/config";
-
-import { toLower } from "helpers/string";
-
-const { publicRuntimeConfig } = getConfig();
-
-export function isAdminAddress(address: string) {
-  return address && toLower(address) === toLower(publicRuntimeConfig?.adminWallet);
-}
