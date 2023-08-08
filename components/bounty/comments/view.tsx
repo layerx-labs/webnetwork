c47217a7211d233f00aef4f11efcc9d164d9ca88
Marcus Vinícius <marcusvinici25@gmail.com>
2023-08-08 15:09:37 -0300

    removing btn reply-on-github

--- components/bounty/comments/view.tsx
+++ components/bounty/comments/view.tsx
@@ -14,24 +14,9 @@ export default function BountyCommentsView({ comments = [], replyRef }) {
         <div className="col">
           <div className="content-wrapper bg-gray-850">
             <div className="d-flex justify-content-between align-items-center mb-3">
-              <h5 className="caption-small mb-0">
-                {t("misc.comments", { count: comments?.length || 0 })}
+              <h5 className="caption-medium mb-0">
+                {t("misc.comments", { count: 0 })}
               </h5>
-              <a
-                href={replyRef}
-                className="text-decoration-none"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <Button 
-                  transparent 
-                  textClass="text-primary"
-                  className="p-0 lh-1"
-                >
-                  <span>{t("actions.reply-on-github")}{" "}</span>
-                  <ExternalLinkIcon color="primary" />
-                </Button>
-              </a>
             </div>
             {!!comments.length && comments?.map((comment) => (
               <Comment comment={comment} key={comment?.id} />
