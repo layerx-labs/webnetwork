bc55c4f0ae6ff734faf701cfdf7ad088d150b0a6
Vitor Hugo <vhcsilvaa@gmail.com>
2023-08-08 08:54:37 -0300

    refactor to use jwt role

--- middleware/with-admin.ts
+++ middleware/with-admin.ts
@@ -1,15 +1,12 @@
 import { NextApiHandler } from "next";
-import getConfig from "next/config";
 
 import { NOT_ADMIN_WALLET } from "helpers/constants";
-import { toLower } from "helpers/string";
 
-import { siweMessageService } from "services/ethereum/siwe";
+import { UserRole } from "interfaces/enums/roles";
+
 import { Logger } from "services/logging";
 
 import { isMethodAllowed } from "server/utils/http";
-
-const { publicRuntimeConfig } = getConfig();
 
 Logger.changeActionName(`withAdmin()`);
 
@@ -18,15 +15,9 @@ export const withAdmin = (handler: NextApiHandler, allowedMethods = ["GET"]): Ne
     if (isMethodAllowed(req.method, allowedMethods))
       return handler(req, res);
 
-    const adminWallet = toLower(publicRuntimeConfig?.adminWallet);
-    const token = req.body?.context?.token;
-    const message = req.body?.context?.typedMessage;
-    const { signature, address } = token;
+    const token = req.body?.context?.token?.roles || [];
 
-    if (!address || toLower(address) !== adminWallet)
-      return res.status(401).json({ message: NOT_ADMIN_WALLET });
-
-    if (!(await siweMessageService.decodeMessage(message, signature?.toString(), adminWallet)))
+    if (!token.includes(UserRole.ADMIN))
       return res.status(401).json({ message: NOT_ADMIN_WALLET });
 
     return handler(req, res);
