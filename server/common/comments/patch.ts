b8774a841b95109d463e86d2dc4299b6d740dd07
Marcus Vinícius <marcusvinici25@gmail.com>
2023-08-08 10:09:30 -0300

    adding new validation to patch

--- server/common/comments/patch.ts
+++ server/common/comments/patch.ts
@@ -15,12 +15,17 @@ export default async function patch(req: NextApiRequest, res: NextApiResponse) {
       },
     });
 
-    if(!comments) return res.status(404).json({ message: 'comment not found'});
+    if (!comments)
+      return res.status(404).json({ message: "comment not found" });
 
-    comments.hidden = hidden;
-    await comments.save();
+    if ([true, false, "true", "false"].includes(hidden)) {
+      comments.hidden = hidden;
+      await comments.save();
 
-    return res.status(200).json(comments);
+      return res.status(200).json(comments);
+    } else {
+      return res.status(404).json({ message: "hidden field is not valid" });
+    }
   } catch (error) {
     LogError(error);
     res.status(500).json(error);
