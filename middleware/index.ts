86f5f66fa2060da4ff1dda00652ae2712ddb98bd
Vitor Hugo <vhcsilvaa@gmail.com>
2023-08-08 10:43:48 -0300

    refactor middlewares

--- middleware/index.ts
+++ middleware/index.ts
@@ -2,18 +2,24 @@ import { NextApiHandler } from "next";
 
 import { LogAccess } from "middleware/log-access";
 import { withAdmin } from "middleware/with-admin";
+import { withGovernor } from "middleware/with-governor";
+import { withIssue } from "middleware/with-issue";
 import { withSignature } from "middleware/with-signature";
 import withCors from "middleware/withCors";
 import { withJWT } from "middleware/withJwt";
 
+const withCORS = (handler: NextApiHandler) => LogAccess(withCors(handler));
 const withProtected = (handler: NextApiHandler) => LogAccess(withCors(withJWT(withSignature(handler))));
 const RouteMiddleware = (handler: NextApiHandler) => LogAccess(withCors(withJWT(handler)));
 const AdminRoute = (handler: NextApiHandler) => withProtected(withAdmin(handler));
+const IssueRoute = (handler: NextApiHandler) => withProtected(withIssue(handler));
 
 export {
-  withCors,
+  withCORS,
   withJWT,
   withProtected,
   RouteMiddleware,
-  AdminRoute
+  AdminRoute,
+  IssueRoute,
+  withGovernor
 };
--- middleware/network-route.ts
+++ middleware/network-route.ts
@@ -3,17 +3,10 @@ import {Op} from "sequelize";
 
 import models from "db/models";
 
-import {
-  IM_AM_CREATOR_NETWORK,
-  NOT_AN_CREATOR_NETWORK,
-  MISSING_CREATOR_NETWORK_SIGNATURE
-} from "helpers/constants";
-import decodeMessage from "helpers/decode-message";
 import { isAdmin } from "helpers/is-admin";
 import { resJsonMessage } from "helpers/res-json-message";
 
 export const NetworkRoute = (handler: NextApiHandler, methods: string[] = [ `PUT` ]) => {
-
   return async (req, res) => {
     if (!methods.includes(req.method.toUpperCase()))
       return handler(req, res);
@@ -42,16 +35,6 @@ export const NetworkRoute = (handler: NextApiHandler, methods: string[] = [ `PUT
     if (!accessToken && isChangingGithubOptions) return resJsonMessage("Unauthorized user", res, 401);
   
     if (!network) return resJsonMessage("Invalid network", res, 401);
-
-    if (!wallet || wallet.toLowerCase() !== network?.creatorAddress.toLowerCase())
-      return resJsonMessage(NOT_AN_CREATOR_NETWORK, res, 401);
-
-    const signature = headers.signature as string;
-    if (!signature)
-      return resJsonMessage(MISSING_CREATOR_NETWORK_SIGNATURE, res, 401);
-
-    if (!decodeMessage(chainId, IM_AM_CREATOR_NETWORK, signature, network?.creatorAddress))
-      return resJsonMessage(NOT_AN_CREATOR_NETWORK, res, 401);
 
     return handler(req, res);
   }
