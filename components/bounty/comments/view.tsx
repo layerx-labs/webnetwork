7ce8782702da5364db395fed68c7d9de6655b431
Marcus Vinícius <marcusvinici25@gmail.com>
2023-08-09 10:04:00 -0300

    adding initial structure to comment component

--- components/bounty/comments/view.tsx
+++ components/bounty/comments/view.tsx
@@ -1,26 +1,66 @@
 import { useTranslation } from "next-i18next";
 
-import ExternalLinkIcon from "assets/icons/external-link-icon";
-
-import Button from "../../button";
-import Comment from "../../comment";
+import Comment, { CommentsProps } from "./comment/view";
+import InputCommentView from "./input-comment/view";
 
 export default function BountyCommentsView({ comments = [], replyRef }) {
   const { t } = useTranslation("common");
+  const newComments: CommentsProps[] = [
+    {
+      id: 1,
+      comment: 'TESTING',
+      hidden: false,
+      type: 'issue',
+      issueId: 1,
+      userId: 1,
+      userAddress: "0x70997970c51812dc3a010c7d01b50e0d17dc79c8",
+      updatedAt: new Date('2023-08-03 16:54:42.977 -0300'),
+      createdAt: new Date('2023-08-03 16:54:42.977 -0300'),
+      user: {
+        id: 1,
+        githubLogin: "MarcusviniciusLsantos",
+        address: "0x70997970c51812dc3a010c7d01b50e0d17dc79c8",
+        createdAt: '2023-08-03 16:54:42.977 -0300',
+        updatedAt: '2023-08-03 16:54:42.977 -0300',
+        githubHandle: "Marcus Vinicius"
+      }
+    },
+    {
+      id: 2,
+      comment: 'TESTING 2',
+      hidden: false,
+      type: 'issue',
+      issueId: 1,
+      userId: 1,
+      userAddress: "0x70997970c51812dc3a010c7d01b50e0d17dc79c8",
+      updatedAt: new Date('2023-08-03 16:54:42.977 -0300'),
+      createdAt: new Date('2023-08-03 16:54:42.977 -0300'),
+      user: {
+        id: 1,
+        githubLogin: "MarcusviniciusLsantos",
+        address: "0x70997970c51812dc3a010c7d01b50e0d17dc79c8",
+        createdAt: '2023-08-03 16:54:42.977 -0300',
+        updatedAt: '2023-08-03 16:54:42.977 -0300',
+        githubHandle: "Marcus Vinicius"
+      }
+    }
+  ]
 
   return (
     <div className="mb-5">
       <div className="row justify-content-center">
         <div className="col">
-          <div className="content-wrapper bg-gray-850">
+          <div className="border-radius-8 p-3 bg-gray-900">
             <div className="d-flex justify-content-between align-items-center mb-3">
               <h5 className="caption-medium mb-0">
-                {t("misc.comments", { count: 0 })}
+                {t("misc.comments")}
               </h5>
             </div>
-            {!!comments.length && comments?.map((comment) => (
-              <Comment comment={comment} key={comment?.id} />
+            {!!newComments.length && newComments?.map((data) => (
+              <Comment {...data} key={data?.id} />
             ))}
+
+            <InputCommentView userAddress="0x70997970c51812dc3a010c7d01b50e0d17dc79c8" />
           </div>
         </div>
       </div>
