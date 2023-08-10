5d2b4a1f55a5fe9b9c5811ebf67f747b94d04331
Marcus Vinícius <marcusvinici25@gmail.com>
2023-08-10 12:52:19 -0300

    adding logic baseline

--- components/bounty/comments/comment/view.tsx
+++ components/bounty/comments/comment/view.tsx
@@ -11,6 +11,7 @@ import { User } from "interfaces/api";
 
 import AvatarOrIdenticon from "../../../avatar-or-identicon";
 import MarkedRender from "../../../MarkedRender";
+import { FlexColumn } from "components/common/flex-box/view";
 
 export interface CommentsProps {
   id: number;
@@ -41,24 +42,30 @@ export default function Comment({
 
   return (
     <div className="border-radius-8 p-3 bg-gray-800 mb-3">
-      <div className="d-flex align-items-center justify-content-between mb-2">
-        <div className="d-flex align-items-center">
-          <AvatarOrIdenticon
-            user={user?.githubLogin}
-            address={userAddress}
-            size="xsm"
-          />
-          <span className="xs-medium ms-2">
-            {user?.githubLogin
-              ? `@${user?.githubLogin}`
-              : truncateAddress(userAddress)}{" "}
-          </span>
-          <span className="p-small text-gray-500 ms-2">
-            {updatedAt && getTimeDifferenceInWords(updatedAt, new Date())}
-          </span>
+      <div className="d-flex align-items-baseline justify-content-between mb-2">
+        <div className="d-flex align-items-baseline">
+          <FlexColumn className="justify-content-center">
+            <AvatarOrIdenticon
+              user={user?.githubLogin}
+              address={userAddress}
+              size="xsm"
+            />
+          </FlexColumn>
+          <FlexColumn className="justify-content-center">
+            <span className="xs-medium ms-2">
+              {user?.githubLogin
+                ? `@${user?.githubLogin}`
+                : truncateAddress(userAddress)}{" "}
+            </span>
+          </FlexColumn>
+          <FlexColumn className="align-items-baseline">
+            <span className="p-small text-gray-500 ms-2">
+              {updatedAt && getTimeDifferenceInWords(updatedAt, new Date())}
+            </span>
+          </FlexColumn>
         </div>
-        <div className="d-flex align-items-center">
-          <span className="mx-2">. . .</span>
+        <div className="d-flex flex-column text-center justify-content-center align-items-center border-radius-4 bg-gray-850">
+          <span className="mx-2 mb-2">. . .</span>
         </div>
       </div>
 
