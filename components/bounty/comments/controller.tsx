391ff1bf470036ebfb50bd315de5f922a7d1fcf6
Marcus Vinícius <marcusvinici25@gmail.com>
2023-08-11 11:36:56 -0300

    adjusting params BountyComments

--- components/bounty/comments/controller.tsx
+++ components/bounty/comments/controller.tsx
@@ -1,9 +1,29 @@
+import { CurrentUserState } from "interfaces/application-state";
+import { IdsComment, TypeComment } from "interfaces/comments";
+import { IssueDataComment } from "interfaces/issue-data";
+
 import BountyCommentsView from "./view";
 
 export default function BountyComments({
   comments = [],
-  userAddress,
-  issueId,
+  currentUser,
+  type,
+  ids,
+  updateData,
+}: {
+  comments: IssueDataComment[];
+  currentUser?: CurrentUserState;
+  type: TypeComment;
+  ids: IdsComment;
+  updateData: (updatePrData?: boolean) => void;
 }) {
-  return <BountyCommentsView comments={comments} userAddress={userAddress} />;
+  return (
+    <BountyCommentsView
+      comments={comments}
+      currentUser={currentUser}
+      type={type}
+      ids={ids}
+      updateData={updateData}
+    />
+  );
 }
--- components/bounty/comments/view.tsx
+++ components/bounty/comments/view.tsx
@@ -2,51 +2,27 @@ import { useTranslation } from "next-i18next";
 
 import ConnectWalletButton from "components/connect-wallet-button";
 
-import Comment, { CommentsProps } from "./comment/view";
+import { CurrentUserState } from "interfaces/application-state";
+import { IdsComment, TypeComment } from "interfaces/comments";
+import { IssueDataComment } from "interfaces/issue-data";
+
+import Comment from "./comment/view";
 import InputComment from "./input-comment/controller";
 
-export default function BountyCommentsView({ comments = [], userAddress }) {
+export default function BountyCommentsView({
+  comments = [],
+  currentUser,
+  type,
+  ids,
+  updateData,
+}: {
+  comments: IssueDataComment[];
+  currentUser?: CurrentUserState;
+  type: TypeComment;
+  ids: IdsComment;
+  updateData: (updatePrData?: boolean) => void;
+}) {
   const { t } = useTranslation("common");
-  const newComments: CommentsProps[] = [
-    {
-      id: 1,
-      comment: "TESTING",
-      hidden: false,
-      type: "issue",
-      issueId: 1,
-      userId: 1,
-      userAddress: "0x70997970c51812dc3a010c7d01b50e0d17dc79c8",
-      updatedAt: new Date("2023-08-03 16:54:42.977 -0300"),
-      createdAt: new Date("2023-08-03 16:54:42.977 -0300"),
-      user: {
-        id: 1,
-        githubLogin: "MarcusviniciusLsantos",
-        address: "0x70997970c51812dc3a010c7d01b50e0d17dc79c8",
-        createdAt: "2023-08-03 16:54:42.977 -0300",
-        updatedAt: "2023-08-03 16:54:42.977 -0300",
-        githubHandle: "Marcus Vinicius",
-      },
-    },
-    {
-      id: 2,
-      comment: "TESTING 2",
-      hidden: false,
-      type: "issue",
-      issueId: 1,
-      userId: 1,
-      userAddress: "0x70997970c51812dc3a010c7d01b50e0d17dc79c8",
-      updatedAt: new Date("2023-08-03 16:54:42.977 -0300"),
-      createdAt: new Date("2023-08-03 16:54:42.977 -0300"),
-      user: {
-        id: 1,
-        githubLogin: "MarcusviniciusLsantos",
-        address: "0x70997970c51812dc3a010c7d01b50e0d17dc79c8",
-        createdAt: "2023-08-03 16:54:42.977 -0300",
-        updatedAt: "2023-08-03 16:54:42.977 -0300",
-        githubHandle: "Marcus Vinicius",
-      },
-    },
-  ];
 
   return (
     <div className="mb-5">
@@ -56,10 +32,16 @@ export default function BountyCommentsView({ comments = [], userAddress }) {
             <div className="d-flex justify-content-between align-items-center mb-3">
               <h5 className="caption-medium mb-0">{t("misc.comments")}</h5>
             </div>
-            {!!newComments.length &&
-              newComments?.map((data) => <Comment {...data} key={data?.id} />)}
-            {userAddress ? (
-              <InputComment userAddress={userAddress} />
+            {!!comments.length &&
+              comments?.map((data) => <Comment {...data} key={data?.id} />)}
+            {currentUser?.walletAddress ? (
+              <InputComment
+                userAddress={currentUser?.walletAddress}
+                githubLogin={currentUser?.login}
+                type={type}
+                ids={ids}
+                updateData={updateData}
+              />
             ) : (
               <div className="d-flex flex-column text-center mt-4 pt-2">
                 <span>{t("comments.not-connect-wallet")}</span>
