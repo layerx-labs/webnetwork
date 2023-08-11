6a81c3d9a42b7668183e31d2e2cb21c0971f7ceb
Marcus Vinícius <marcusvinici25@gmail.com>
2023-08-11 11:38:43 -0300

    adding submitComment logic to InputComment

--- components/bounty/comments/input-comment/controller.tsx
+++ components/bounty/comments/input-comment/controller.tsx
@@ -1,24 +1,57 @@
 import { ChangeEvent, useState } from "react";
 
+import { useTranslation } from "next-i18next";
+
+import { useAppState } from "contexts/app-state";
+import { addToast } from "contexts/reducers/change-toaster";
+
+import { IdsComment, TypeComment } from "interfaces/comments";
+
+import CreateComment from "x-hooks/api/comments/post-comments";
+
 import InputCommentView from "./view";
+
 
 export default function InputComment({
   githubLogin,
   userAddress,
+  type,
+  ids,
+  updateData
 }: {
   githubLogin?: string;
   userAddress: string;
+  type: TypeComment;
+  ids: IdsComment;
+  updateData: (updatePrData?: boolean) => void;
 }) {
+  const { t } = useTranslation(["common", "bounty"]);
   const [comment, setComment] = useState<string>();
+  const { dispatch } = useAppState();
 
   function handleChangeComment(e: ChangeEvent<HTMLTextAreaElement>) {
     setComment(e.target.value)
   }
 
-  function handleSubmitComment() {
-    console.log('commment', comment)
-    //comment.post(comment)
-    //updateData()
+  async function handleSubmitComment() {
+    await CreateComment({
+      comment,
+      ...ids,
+      type
+    }).then(() => {
+      dispatch(addToast({
+        type: "success",
+        title: t("actions.success"),
+        content: t("bounty:actions.comment.success"),
+      }));
+      updateData()
+    }).catch(() => {
+      dispatch(addToast({
+        type: "danger",
+        title: t("actions.success"),
+        content: t("bounty:actions.comment.error"),
+      }));
+    })
   }
 
   return (
