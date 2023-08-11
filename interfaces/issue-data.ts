ce7088ddc926b64bbfbd32ac4880a71fd3440436
Marcus Vinícius <marcusvinici25@gmail.com>
2023-08-11 11:40:40 -0300

    creating comments interfacee

--- interfaces/issue-data.ts
+++ interfaces/issue-data.ts
@@ -5,6 +5,8 @@ import { Payment } from "interfaces/payments";
 import { Proposal, INetworkProposal } from "interfaces/proposal";
 import { Token } from "interfaces/token";
 
+import { User } from "./api";
+
 export type IssueState =
   | "pending"
   | "draft"
@@ -145,12 +147,20 @@ export interface developer {
   url?: string;
   type?: string;
 }
-
 export interface IssueDataComment {
-  body: string;
-  created_at: string | number | Date;
-  updated_at: string | number | Date;
-  author: string;
+  id: number;
+  comment: string;
+  hidden: boolean;
+  type: string;
+  issueId: number;
+  proposalId?: number;
+  deliberableId?: number;
+  userId: number;
+  userAddress: string;
+  replyId?: number;
+  updatedAt: Date;
+  createdAt: Date;
+  user: User;
 }
 
 export interface GithubUser {
