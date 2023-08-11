2535ed1b8655cbff1be2175163e6da9d5dcc4da3
Marcus Vinícius <marcusvinici25@gmail.com>
2023-08-11 13:54:12 -0300

    adding hide api/hook

---
+++ x-hooks/api/comments/patch-comments.ts
@@ -0,0 +1,17 @@
+import { IssueDataComment } from "interfaces/issue-data";
+
+import { api } from "services/api";
+
+/**
+ * Hide comments from api
+ * @param query current url query
+ * @returns comment
+ */
+export default async function HideComment(id: number,
+                                          payload: {
+    hidden: boolean;
+  }) {
+  return api.patch<IssueDataComment>(`/comments/${id}`, {
+    ...payload,
+  });
+}
