694e95de12bf87c404cf2c9b2923977c373d12e2
Marcus Vinícius <marcusvinici25@gmail.com>
2023-08-11 12:59:23 -0300

    adding post comments api/hook

---
+++ x-hooks/api/comments/post-comments.ts
@@ -0,0 +1,20 @@
+import { IssueDataComment } from "interfaces/issue-data";
+
+import { api } from "services/api";
+
+/**
+ * Create comment in api
+ * @param query current url query
+ * @returns created comment
+ */
+export default async function CreateComment(payload: {
+  comment: string;
+  issueId: number;
+  type: "deliverable" | "issue" | "proposal";
+  deliverableId?: number;
+  proposalId?: number;
+}) {
+  return api.post<IssueDataComment>(`/comments`, {
+    ...payload,
+  });
+}
