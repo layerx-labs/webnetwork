758c1bfaf23ba7eda33f02a82dbaacbfa364341c
Marcus Vinícius <marcusvinici25@gmail.com>
2023-08-16 17:16:07 -0300

    removing ocktokit post issue and adjusting logic

--- pages/api/issue/index.ts
+++ pages/api/issue/index.ts
@@ -1,12 +1,7 @@
 import {NextApiRequest, NextApiResponse} from "next";
-import getConfig from "next/config";
-import {Octokit} from "octokit";
 import {Op} from "sequelize";
 
 import models from "db/models";
-
-import * as IssueQueries from "graphql/issue";
-import * as RepositoryQueries from "graphql/repository";
 
 import {chainFromHeader} from "helpers/chain-from-header";
 
@@ -14,13 +9,8 @@ import { LogAccess } from "middleware/log-access";
 import {WithValidChainId} from "middleware/with-valid-chain-id";
 import WithCors from "middleware/withCors";
 
-import {GraphQlResponse} from "types/octokit";
-
-const {serverRuntimeConfig} = getConfig();
-
 async function post(req: NextApiRequest, res: NextApiResponse) {
   const {
-    title,
     body,
     repositoryId,
     networkName,
@@ -51,62 +41,7 @@ async function post(req: NextApiRequest, res: NextApiResponse) {
 
   if (!repository) return res.status(404).json("Repository not found");
 
-  const [owner, repo] = repository.githubPath.split("/");
-
-  const githubAPI = (new Octokit({ auth: serverRuntimeConfig?.github?.token })).graphql;
-
-  const repositoryDetails = await githubAPI<GraphQlResponse>(RepositoryQueries.Details, {
-    repo,
-    owner
-  });
-
-  const repositoryGithubId = repositoryDetails.repository.id;
-  
-  let draftLabelId = repositoryDetails.repository.labels.nodes.find(({ name }) => name.toLowerCase() === "draft")?.id;
-  let chainLabelId = repositoryDetails.repository.labels.nodes.
-    find(({ name }) => name.toLowerCase() === chain.chainName.toLowerCase())?.id;
-
-  if (!draftLabelId) {
-    const createdLabel = await githubAPI<GraphQlResponse>(RepositoryQueries.CreateLabel, {
-      name: "draft",
-      repositoryId: repositoryGithubId,
-      color: "cfd3d7",
-      headers: {
-        accept: "application/vnd.github.bane-preview+json"
-      }
-    });
-
-    draftLabelId = createdLabel.createLabel.label.id;
-  }
-
-  if (!chainLabelId) {
-    const createdLabel = await githubAPI<GraphQlResponse>(RepositoryQueries.CreateLabel, {
-      name: chain.chainName,
-      repositoryId: repositoryGithubId,
-      color: chain.color.replace("#", ""),
-      headers: {
-        accept: "application/vnd.github.bane-preview+json"
-      }
-    });
-
-    chainLabelId = createdLabel.createLabel.label.id;
-  }
-
-  const createdIssue = await githubAPI<GraphQlResponse>(IssueQueries.Create, {
-    repositoryId: repositoryGithubId,
-    title,
-    body,
-    labelId: [draftLabelId, chainLabelId]
-  });
-
-  const githubId = createdIssue.createIssue.issue.number;
-
-  if (await models.issue.findOne({ where: { githubId: `${githubId}`, repository_id: repository.id } }))
-    return res.status(409).json("issueId already exists on database");
-
-  await models.issue.create({
-    issueId: `${repository.id}/${githubId}`,
-    githubId: `${githubId}`,
+  const issue = await models.issue.create({
     repository_id: repository.id,
     creatorAddress: '',
     creatorGithub: '',
@@ -122,7 +57,7 @@ async function post(req: NextApiRequest, res: NextApiResponse) {
     kycTierList: tierList?.map(Number).filter(id=> !Number.isNaN(id)) || [],
   });
 
-  return res.status(200).json(`${repository.id}/${githubId}`);
+  return res.status(200).json(`${repository.id}/${issue.id}`);
 }
 
 export default LogAccess(WithCors(WithValidChainId(async function Issue(req: NextApiRequest, res: NextApiResponse) {
