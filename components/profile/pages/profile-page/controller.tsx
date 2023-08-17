bb3196cbfa0357fc132fb61868cdbd97c4848751
vhcsilva <vhcsilvaa@gmail.com>
2023-08-17 11:55:45 -0300

    add email configuration

--- components/profile/pages/profile-page/controller.tsx
+++ components/profile/pages/profile-page/controller.tsx
@@ -1,31 +1,99 @@
-import React, { useState } from "react";
+import { useState, useEffect } from "react";
+
+import { useSession } from "next-auth/react";
+import { useDebouncedCallback } from "use-debounce";
+
+import ProfilePageView from "components/profile/pages/profile-page/view";
 
 import { useAppState } from "contexts/app-state";
 
+import { lowerCaseCompare } from "helpers/string";
+import { isValidEmail } from "helpers/validators/email";
+
+import useApi from "x-hooks/use-api";
 import { useAuthentication } from "x-hooks/use-authentication";
 
-import ProfilePageView from "./view";
-
 export default function ProfilePage() {
+  const { data: session, status, update: updateSession } = useSession();
 
+  const [inputEmail, setInputEmail] = useState("");
+  const [isExecuting, setIsExecuting] = useState(false);
+  const [isEmailInvalid, setIsEmailInvalid] = useState(false);
   const [showRemoveModal, setShowRemoveModal] = useState(false);
+  const [isNotificationEnabled, setIsNotificationEnabled] = useState(false);
+
+  const emailValidator = useDebouncedCallback(email => {
+    setIsEmailInvalid(email !== "" && !isValidEmail(email));
+  }, 500);
 
   const { state } = useAppState();
-
+  const { updateUserEmail } = useApi();
   const { signOut } = useAuthentication();
+
+  const userEmail = session?.user?.email || "";
+  const isSameEmail = lowerCaseCompare(userEmail, inputEmail);
 
   const handleClickDisconnect = () => setShowRemoveModal(true);
   const hideRemoveModal = () => setShowRemoveModal(false);
 
+  function handleEmailChange(e) {
+    setInputEmail(e.target.value);
+    emailValidator(e.target.value);
+  }
+
+  function handleUpdateEmail(email: string) {
+    setIsExecuting(true);
+
+    updateUserEmail(email)
+      .catch(error => {
+        console.debug("Failed to update user email", error);
+      })
+      .finally(() => {
+        setIsExecuting(false);
+        updateSession();
+      });
+  }
+
+  function onSave() {
+    handleUpdateEmail(inputEmail);
+  }
+
+  function onSwitchChange(newValue: boolean) {
+    if (!newValue) {
+      if (!isExecuting && userEmail !== "")
+        handleUpdateEmail("");
+      else
+        setIsNotificationEnabled(false);
+    } else
+      setIsNotificationEnabled(true);
+  }
+
+  useEffect(() => {
+    if (status === "authenticated") {
+      setInputEmail(userEmail);
+
+      if (!!userEmail !== isNotificationEnabled) 
+        setIsNotificationEnabled(!!userEmail);
+    }
+  }, [session, status]);
+
   return (
     <ProfilePageView
       userLogin={state.currentUser?.login}
+      userEmail={inputEmail}
+      onSave={onSave}
+      isSaveButtonDisabled={isSameEmail || isExecuting || isEmailInvalid}
+      isSwitchDisabled={isExecuting}
+      isEmailInvalid={isEmailInvalid}
+      handleEmailChange={handleEmailChange}
+      isNotificationEnabled={isNotificationEnabled}
       walletAddress={state.currentUser?.walletAddress}
       isCouncil={state.Service?.network?.active?.isCouncil}
       handleClickDisconnect={handleClickDisconnect}
       hideRemoveModal={hideRemoveModal}
       showRemoveModal={showRemoveModal}
       disconnectGithub={signOut}
+      onSwitchChange={onSwitchChange}
     />
   );
 }
--- components/profile/pages/profile-page/view.tsx
+++ components/profile/pages/profile-page/view.tsx
@@ -4,40 +4,60 @@ import { useTranslation } from "next-i18next";
 
 import AvatarOrIdenticon from "components/avatar-or-identicon";
 import Badge from "components/badge";
+import Button from "components/button";
+import Switch from "components/common/switch/view";
 import GithubConnectionState from "components/connections/github-connection-state/controller";
 import CustomContainer from "components/custom-container";
+import { Divider } from "components/divider";
+import If from "components/If";
+import AddressWithCopy from "components/profile/address-with-copy/controller";
 import ProfileLayout from "components/profile/profile-layout";
 import { RemoveGithubAccount } from "components/profile/remove-github-modal";
 import ResponsiveWrapper from "components/responsive-wrapper";
-
-import { truncateAddress } from "helpers/truncate-address";
 
 import useBreakPoint from "x-hooks/use-breakpoint";
 
 interface ProfilePageViewProps {
   userLogin: string;
+  userEmail?: string;
+  isNotificationEnabled: boolean;
   walletAddress: string;
   isCouncil: boolean;
+  showRemoveModal: boolean;
+  isSaveButtonDisabled: boolean;
+  isSwitchDisabled: boolean;
+  isEmailInvalid: boolean;
   handleClickDisconnect: () => void;
   hideRemoveModal: () => void;
-  showRemoveModal: boolean;
   disconnectGithub: () => void;
+  handleEmailChange: (e) => void;
+  onSave: () => void;
+  onSwitchChange: (value: boolean) => void;
 }
 
 export default function ProfilePageView({
   userLogin,
   walletAddress,
+  userEmail,
+  isNotificationEnabled,
   isCouncil,
+  showRemoveModal,
+  isSaveButtonDisabled,
+  isSwitchDisabled,
+  isEmailInvalid,
   handleClickDisconnect,
   hideRemoveModal,
   disconnectGithub,
-  showRemoveModal,
+  handleEmailChange,
+  onSave,
+  onSwitchChange,
 }: ProfilePageViewProps) {
   const { t } = useTranslation(["common", " profile"]);
 
   const { isMobileView, isTabletView } = useBreakPoint();
 
   const isTabletOrMobile = isMobileView || isTabletView ? true : false;
+  const handleClasses = "text-white xl-semibold font-weight-medium text-truncate";
 
   return (
     <>
@@ -50,7 +70,7 @@ export default function ProfilePageView({
       </div>
 
       <ProfileLayout>
-        <div className="row mb-5">
+        <div className="row mb-4">
           <div className="col">
             <div
               className={`${
@@ -61,15 +81,31 @@ export default function ProfilePageView({
                 user={userLogin}
                 address={walletAddress}
                 size={isTabletOrMobile ? "md" : "lg"}
+                withBorder
               />
-              <div className="text-truncate">
-                <h4
-                  className={`${
-                    isTabletOrMobile ? "ms-2" : "mt-2"
-                  } text-gray-100 font-weight-medium text-uppercase text-truncate mr-2`}
+              <div className={`d-flex flex-column ${isTabletOrMobile ? "ms-2" : "mt-2" }`}>
+                <If
+                  condition={!!userLogin}
+                  otherwise={
+                    <AddressWithCopy
+                      address={walletAddress}
+                      textClass={handleClasses}
+                      truncated
+                    />
+                  }
                 >
-                  {userLogin ? userLogin : truncateAddress(walletAddress)}
-                </h4>
+                  <span className={handleClasses}>
+                    {userLogin}
+                  </span>
+                </If>
+
+                <If condition={!!userLogin}>
+                  <AddressWithCopy
+                    address={walletAddress}
+                    textClass="caption-medium font-weight-normal text-capitalize text-gray-300"
+                    truncated
+                  />
+                </If>
               </div>
             </div>
             {isCouncil && (
@@ -82,7 +118,51 @@ export default function ProfilePageView({
           </div>
         </div>
 
-        <div className="row mb-3">
+        <Divider bg="gray-850" />
+
+        <div className="row mb-4 mt-4">
+          <div className="col-8">
+            <div className="d-flex align-items-center justify-content-between mb-1">
+              <span className="base-medium text-white">Notifications</span>
+
+              <Switch
+                value={isNotificationEnabled}
+                onChange={onSwitchChange}
+                disabled={isSwitchDisabled}
+              />  
+            </div>
+
+            <div className="row">
+              <span className="text-gray-500 xs-medium font-weight-normal">
+                Allow Bepro Network to send you updates via email. These can include updates to bounties, app updates, etc.
+              </span>
+            </div>
+
+            <If condition={isNotificationEnabled}>
+              <div className="row mt-3">
+                <div className="col-12 col-md-6">
+                  <input type="email" className="form-control" value={userEmail} onChange={handleEmailChange} />
+                  <If condition={isEmailInvalid}>
+                    <small>Invalid email</small>
+                  </If>
+                </div>
+
+                <div className="col-12 col-md-6">
+                  <Button
+                    onClick={onSave}
+                    disabled={isSaveButtonDisabled}
+                  >
+                    Save
+                  </Button>
+                </div>
+              </div>
+            </If>
+          </div>
+        </div>
+
+        <Divider bg="gray-850" />
+
+        <div className="row mt-4 mb-3">
           <span className="caption text-white text-capitalize font-weight-medium">{t("profile:connections")}</span>
         </div>
 
