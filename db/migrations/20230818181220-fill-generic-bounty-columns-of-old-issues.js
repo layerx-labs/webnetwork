651e4ee802c3142f86d7a36984027b1dc2453515
vhcsilva <vhcsilvaa@gmail.com>
2023-08-21 09:05:00 -0300

    add migration to create ipfs data

---
+++ db/migrations/20230818181220-fill-generic-bounty-columns-of-old-issues.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const { default: BigNumber } = require("bignumber.js");
+const { getAllFromTable } = require("../../helpers/db/rawQueries");
+const { sendToIpfs } = require("../../helpers/db/ipfs");
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    const issues = await getAllFromTable(queryInterface, "issues");
+    const repositories = await getAllFromTable(queryInterface, "repositories");
+    const networks = await getAllFromTable(queryInterface, "networks");
+
+    let repository = null;
+    let network = null;
+
+    const findRepository = id => repositories.find(repo => repo.id === id);
+    const findNetwork = id => networks.find(repo => repo.id === id);
+
+    for (const issue of issues) {
+      if (issue.repository_id !== repository?.id)
+        repository = findRepository(issue.repository_id);
+
+      if (issue.network_id !== network?.id)
+        network = findNetwork(issue.network_id);
+
+      const [owner, repo] = repository?.githubPath?.split("/");
+
+      const bountyJson = {
+        name: issue.title,
+        properties: {
+          type: "github",
+          deliverable: `https://github.com/${owner}/${repo}#${issue.branch}`,
+          bountyId: issue.contractId,
+          chainId: issue.chain_id,
+          networkName: network.name,
+          price: BigNumber.max(issue.amount, issue.fundingAmount).toString(),
+          tags: issue.tags,
+          kycNeeded: issue.isKyc
+        }
+      };
+
+      const ipfsHash = await sendToIpfs(bountyJson, true);
+
+      await queryInterface.bulkUpdate("issues", {
+        type: "github",
+        ipfsUrl: ipfsHash
+      }, {
+        id: issue.id
+      });
+    }
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkUpdate("issues", {
+      type: null,
+      ipfsUrl: null
+    });
+  }
+};
