42bcef079f6773512baeb8ba460c51ffe6a12346
Marcus Vinícius <marcusvinici25@gmail.com>
2023-08-23 15:35:25 -0300

    adding new logic session

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -79,7 +79,7 @@ export default async function auth(req: NextApiRequest, res: NextApiResponse) {
         return params?.token;
       },
       async session({ session, token }) {
-        const { login, accessToken, roles, address, nonce } = token;
+        const { login, roles, address, nonce } = token;
 
         const accountsMatch = await AccountValidator.matchAddressAndGithub(address?.toString(), login?.toString());
         const user = await models.user.scope("ownerOrGovernor").findByAddress(address);
@@ -87,8 +87,7 @@ export default async function auth(req: NextApiRequest, res: NextApiResponse) {
         return {
           expires: session.expires,
           user: {
-            login,
-            accessToken,
+            login: user.githubLogin,
             roles,
             address,
             accountsMatch,
