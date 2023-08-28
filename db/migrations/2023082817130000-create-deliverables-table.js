c0e66123223c274fcaa6dad6462c447e93e5f4c2
Marcus Vinícius <marcusvinici25@gmail.com>
2023-08-28 17:22:28 -0300

    creating migration deliverables table

---
+++ db/migrations/2023082817130000-create-deliverables-table.js
@@ -0,0 +1,69 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("deliverables", {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        unique: true
+      },
+      deliverableUrl: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      ipfsLink: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      description: {
+        type: Sequelize.DataTypes.TEXT,
+        allowNull: false
+      },
+      canceled: {
+        type: Sequelize.BOOLEAN, 
+        defaultValue: false
+      },
+      markedReadyForReview: {
+        type: Sequelize.BOOLEAN, 
+        defaultValue: false
+      },
+      accepted: {
+        type: Sequelize.BOOLEAN, 
+        defaultValue: false
+      },
+      issueId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "issues",
+          key: "id"
+        }
+      },
+      bountyId: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "users",
+          key: "id"
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("deliverables");
+  }
+};
