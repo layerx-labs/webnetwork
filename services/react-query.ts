61f1af5d9a1f76ac3ee02a486afa2ff01b020758
vhcsilva <vhcsilvaa@gmail.com>
2023-09-04 11:15:38 -0300

    remove refetch options

--- services/react-query.ts
+++ services/react-query.ts
@@ -4,9 +4,6 @@ export const reactQueryConfig: QueryClientConfig = {
   defaultOptions: {
     queries: {
       staleTime: 1000 * 60 * 10, // 10s
-      refetchOnWindowFocus: false,
-      refetchOnMount: false,
-      refetchOnReconnect: false,
     },
   }
 };
