08a61d5d91a7fbc3345bcd2c8efc783972dab529
vhcsilva <vhcsilvaa@gmail.com>
2023-09-05 16:17:29 -0300

    refactor profile page data getter function

--- pages/[network]/[chain]/profile/[[...profilePage]].tsx
+++ pages/[network]/[chain]/profile/[[...profilePage]].tsx
@@ -5,14 +5,25 @@ import ProfileRouter from "components/profile/profile-router";
 
 import { ProfilePageProps } from "types/pages";
 
-import getProfilePageData from "x-hooks/api/get-profile-data";
+import { useGetProfileBounties, useGetProfilePayments } from "x-hooks/api/pages/profile";
 
 export default function Profile(props: ProfilePageProps) {
   return <ProfileRouter {...props} />;
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ query, locale }) => {
-  const pageData = await getProfilePageData(query);
+  const { profilePage } = query || {};
+  const [pageName] = (profilePage || ["profile"]);
+
+  const getDataFn = {
+    payments: () => useGetProfilePayments(query),
+    bounties: () => useGetProfileBounties(query, "creator"),
+    proposals: () => useGetProfileBounties(query, "proposer"),
+    "pull-requests": () => useGetProfileBounties(query, "pullRequester"),
+    "my-network": () => useGetProfileBounties(query, "governor"),
+  };
+
+  const pageData = await getDataFn[pageName]();
   
   return {
     props: {
