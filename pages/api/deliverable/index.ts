dc51f48203d9e73e644c58a4281f94263198d40a
Marcus Vinícius <marcusvinici25@gmail.com>
2023-09-06 18:37:32 -0300

    adding deliverable handler

---
+++ pages/api/deliverable/index.ts
@@ -0,0 +1,24 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+import { withCORS, WithValidChainId } from "middleware";
+
+import get from "server/common/deliverable/get";
+import post from "server/common/deliverable/post";
+
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  switch (req.method) {
+  case "GET":
+    await get(req, res);
+    break;
+  case "POST":
+    await post(req, res);
+    break;
+
+  default:
+    res.status(405);
+  }
+
+  res.end();
+}
+export default withCORS(WithValidChainId(handler));
