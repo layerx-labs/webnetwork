8093cb64b4ec1decbc1fdd62d382d605228c9425
Marcus Vinícius <marcusvinici25@gmail.com>
2023-09-11 12:07:46 -0300

    adjusting route delete to use user from withUser

--- server/common/deliverable/delete.ts
+++ server/common/deliverable/delete.ts
@@ -4,23 +4,25 @@ import models from "db/models";
 
 import { resJsonMessage } from "helpers/res-json-message";
 
+import { UserRoleUtils } from "server/utils/jwt";
+
 export default async function del(req: NextApiRequest, res: NextApiResponse) {
   const { id } = req.query;
 
   const { context } = req.body;
 
-  const user = await models.user.findByAddress(context.token.address)
+  const isGovernor = UserRoleUtils.hasGovernorRole(context.token);
 
-  if (!user) return res.status(404).json({ message: "user not found" });
+  console.log('isGovernor', isGovernor)
 
   const deliverable = await models.deliverable.findOne({
     where: {
       id: id,
-      userId: user.id
+      ... isGovernor ? {} : {userId: context.user.id}
     },
   });
 
-  if (!deliverable) return resJsonMessage("Invalid", res, 404);
+  if (!deliverable) return resJsonMessage("Invalid", res, 409);
 
   if (deliverable.prContractId)
     return resJsonMessage("This deliverable already exists in the contract",
