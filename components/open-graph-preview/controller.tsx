61064d1c899baa67a9db4a90c848f87f3722798a
vhcsilva <vhcsilvaa@gmail.com>
2023-09-12 15:31:18 -0300

    refactor and add translations

--- components/open-graph-preview/controller.tsx
+++ components/open-graph-preview/controller.tsx
@@ -1,6 +1,4 @@
-import ArrowUpRight from "assets/icons/arrow-up-right";
-
-import If from "components/If";
+import OpenGraphPreviewView from "components/open-graph-preview/view";
 
 import { MINUTE_IN_MS } from "helpers/constants";
 import { isValidUrl } from "helpers/validateUrl";
@@ -30,52 +28,14 @@ export default function OpenGraphPreview({
   const isImage = data?.ogImage ? true : false;
 
   return(
-    <div className="d-flex p-3 flex-column align-items-center border border-radius-4 border-gray-800 comment">
-      <If
-        condition={!isFetching}
-        otherwise={<span className="spinner-border spinner-border m-5" />}
-      >
-        <If
-          condition={isImage}
-        >
-          
-        </If>
-
-        <If
-          condition={!!preview}
-          otherwise={
-            <span className="p-5 sm-regular text-gray-600">
-              { previewPlaceholder || "No preview available" }
-            </span>
-          }
-        >
-          <If
-            condition={isImage}
-            otherwise={
-              <video src={preview} controls></video>
-            }
-          >
-            <img src={preview} className="border-radius-8" />
-          </If>
-        </If>
-
-        <If condition={showOpenLink && !!preview}>
-          <div className="w-100 text-left mt-3">
-            <a
-              href={url}
-              target="_blank"
-              rel="noopener noreferer" 
-              className="sm-regular text-decoration-none text-blue-200"
-            >
-              <span className="mr-1">
-                { openLinkText || "View Link" }
-              </span>
-
-              <ArrowUpRight />
-            </a>
-          </div>
-        </If>
-      </If>
-    </div>
+    <OpenGraphPreviewView
+      isFetching={isFetching}
+      isImage={isImage}
+      previewPlaceholder={previewPlaceholder}
+      preview={preview}
+      showOpenLink={showOpenLink}
+      url={url}
+      openLinkText={openLinkText}
+    />
   );
 }
