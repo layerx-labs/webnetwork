584a3d0fdc70c32379a38ea82d69f273fb657a5c
vhcsilva <vhcsilvaa@gmail.com>
2023-09-15 09:36:09 -0300

    use URL because protocol is required

--- helpers/validateUrl.ts
+++ helpers/validateUrl.ts
@@ -1,3 +1,8 @@
 export function isValidUrl(url: string) {
-  return /^((http|https):\/\/.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)$/.test(url);
+  try {
+    new URL(url);
+    return true;
+  } catch (err) {
+    return false;
+  }
 }
