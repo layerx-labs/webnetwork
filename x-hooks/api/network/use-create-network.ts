2e1793a5ad8d34703506bbfa63c1427612f02413
vhcsilva <vhcsilvaa@gmail.com>
2023-09-15 11:19:12 -0300

    refactor export

--- x-hooks/api/network/use-create-network.ts
+++ x-hooks/api/network/use-create-network.ts
@@ -2,7 +2,7 @@ import { api } from "services/api";
 
 import { CreateNetworkParams } from "types/api";
 
-export default async function useCreateNetwork(payload: CreateNetworkParams): Promise<number> {
+export async function useCreateNetwork(payload: CreateNetworkParams): Promise<number> {
   return api
     .post("/network", payload)
     .then(({ data }) => data);
