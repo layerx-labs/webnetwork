aefa225a5bce2b2ad64e558b15dd5eed654e26b8
vhcsilva <vhcsilvaa@gmail.com>
2023-09-20 14:00:53 -0300

    use mutation

--- components/custom-network/new-network-stepper.tsx
+++ components/custom-network/new-network-stepper.tsx
@@ -41,24 +41,26 @@ import { useCreateNetwork } from "x-hooks/api/network";
 import useBepro from "x-hooks/use-bepro";
 import {useNetwork} from "x-hooks/use-network";
 import useNetworkTheme from "x-hooks/use-network-theme";
+import useReactQueryMutation from "x-hooks/use-react-query-mutation";
 import useSignature from "x-hooks/use-signature";
 
 function NewNetwork() {
   const router = useRouter();
-
   const { t } = useTranslation(["common", "custom-network"]);
 
   const [hasNetwork, setHasNetwork] = useState(false);
   const [creatingNetwork, setCreatingNetwork] = useState<number>(-1);
 
-  const { state, dispatch } = useAppState();
-
   const { signMessage } = useSignature();
+  const { state, dispatch } = useAppState();
   const { colorsToCSS } = useNetworkTheme();
   const { getURLWithNetwork } = useNetwork();
   const { processEvent } = useProcessEvent();
   const { handleDeployNetworkV2, handleAddNetworkToRegistry, handleChangeNetworkParameter } = useBepro();
   const { tokensLocked, details, tokens, settings, isSettingsValidated, cleanStorage } = useNetworkSettings();
+  const { mutateAsync: createNetwork } = useReactQueryMutation({
+    mutationFn: useCreateNetwork
+  });
 
   const isSetupPage = router?.pathname?.toString()?.includes("setup");
 
@@ -108,7 +110,7 @@ function NewNetwork() {
       signedMessage
     };
 
-    const networkCreated = await useCreateNetwork(payload)
+    const networkCreated = await createNetwork(payload)
       .catch((error) => {
         setCreatingNetwork(-1);
         dispatch(addToast({
