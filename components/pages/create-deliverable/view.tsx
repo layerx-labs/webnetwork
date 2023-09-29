6ad6e254dbab9bcb53324ffcacabe19ba8c8f834
Marcus Vinícius <marcusvinici25@gmail.com>
2023-09-29 09:12:48 -0300

    adding originLinkPlaceholder to deliverable

--- components/pages/create-deliverable/view.tsx
+++ components/pages/create-deliverable/view.tsx
@@ -23,6 +23,7 @@ interface CreateDeliverablePageViewProps {
   checkButtonsOptions: SelectOption[];
   checkButtonsOption: string;
   originLink: string;
+  originLinkPlaceHolder: string;
   onChangeOriginLink: (v: ChangeEvent<HTMLInputElement>) => void;
   description: string;
   onChangeDescription: (e: ChangeEvent<HTMLTextAreaElement>) => void;
@@ -39,6 +40,7 @@ interface CreateDeliverablePageViewProps {
 
 export default function CreateDeliverablePageView({
   originLink,
+  originLinkPlaceHolder,
   onChangeOriginLink,
   title,
   onChangeTitle,
@@ -108,7 +110,7 @@ export default function CreateDeliverablePageView({
                     "border border-1 border-danger border-radius-8":
                       previewError || originLinkError,
                   })}
-                  placeholder={t("deliverable:create.placeholders.origin-link")}
+                  placeholder={originLinkPlaceHolder}
                   value={originLink}
                   onChange={onChangeOriginLink}
                 />
