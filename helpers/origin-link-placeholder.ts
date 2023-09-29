7a55fbef4f4cc4f460d9c8b078c58b1facdd9080
Marcus Vinícius <marcusvinici25@gmail.com>
2023-09-29 11:36:54 -0300

    adjusting getOriginLinkPlaceholder function

--- helpers/origin-link-placeholder.ts
+++ helpers/origin-link-placeholder.ts
@@ -2,11 +2,14 @@ import { TFunction } from "next-i18next";
 
 //The translation must have "bounty" initialized
 export function getOriginLinkPlaceholder(translation: TFunction, type: string) {
-  const placeholderPath = "bounty:fields.origin-link.placeholders"
+  const placeholderPath = "bounty:fields.origin-link.placeholders";
 
-  if(type === 'code') return translation(`${placeholderPath}.code`)
-  if(type === 'design') return translation(`${placeholderPath}.design`)
-  if(type === 'other') return translation(`${placeholderPath}.other`)
+  const placeholder =
+    {
+      code: translation(`${placeholderPath}.code`),
+      design: translation(`${placeholderPath}.design`),
+      other: translation(`${placeholderPath}.other`),
+    }[type] || translation(`${placeholderPath}.default`);
 
-  return  translation(`${placeholderPath}.default`)
+  return placeholder;
 }
