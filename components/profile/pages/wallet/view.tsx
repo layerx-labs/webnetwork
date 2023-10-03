c69cd0397fab5d9eae9bab23403f3b82881b749d
Marcus Vinícius <marcusvinici25@gmail.com>
2023-10-03 15:35:26 -0300

    adjusting tokens and chains to WalletPage

--- components/profile/pages/wallet/view.tsx
+++ components/profile/pages/wallet/view.tsx
@@ -2,14 +2,19 @@ import {Col, Row} from "react-bootstrap";
 
 import ProfileLayout from "components/profile/profile-layout";
 
+import { WalletPageProps } from "types/pages";
+
 import WalletBalance from "./wallet-balance/controller";
 
-export default function WalletPage() {
+export default function WalletPage({
+  tokens,
+  chains
+}: WalletPageProps) {
   return (
     <ProfileLayout>
       <Col xs={12}>
         <Row className="mb-3">
-          <WalletBalance/>
+          <WalletBalance dbTokens={tokens} chains={chains}/>
         </Row>
       </Col>
     </ProfileLayout>
