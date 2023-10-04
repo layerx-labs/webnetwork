3c2e408c2372fac672ffc216d1251faba0282c54
Marcus Vinícius <marcusvinici25@gmail.com>
2023-10-04 18:41:59 -0300

    adding AmountTokenInformation to token-amount

--- components/bounty/create-bounty/token-amount/view.tsx
+++ components/bounty/create-bounty/token-amount/view.tsx
@@ -3,6 +3,7 @@ import { NumberFormatValues } from "react-number-format";
 import BigNumber from "bignumber.js";
 import { useTranslation } from "next-i18next";
 
+import AmountTokenInformation from "components/bounty/amount-token-information/view";
 import ResponsiveWrapper from "components/responsive-wrapper";
 
 import { DistributionsProps } from "interfaces/proposal";
@@ -12,7 +13,6 @@ import InputNumber from "../../../input-number";
 import TokensDropdown from "../../../tokens-dropdown";
 import RewardInformationBalanceView from "../sections/reward-information/balance/view";
 import ServiceFeesModalView from "../service-fees-modal/view";
-import RenderItemRow from "./item-row/view";
 
 interface CreateBountyTokenAmountViewProps {
   currentToken: Token;
@@ -133,53 +133,18 @@ export default function CreateBountyTokenAmountView({
           </ResponsiveWrapper>
         </div>
 
-        <RenderItemRow
-          label={
-            isFunding
-              ? t("bounty:fields.select-token.reward")
-              : t("bounty:fields.select-token.bounty")
-          }
-          description={
-            isFunding
-              ? t("bounty:set-funded-reward-description")
-              : t("bounty:set-reward-description")
-          }
-        >
-          <InputNumber
-            symbol={currentToken?.symbol}
-            classSymbol=""
-            thousandSeparator
-            value={rewardAmount?.value}
-            placeholder="0"
-            max={tokenBalance.toFixed()}
-            allowNegative={false}
-            decimalScale={decimals}
-            onValueChange={(e) =>
-              e.value !== rewardAmount.value &&
-              onIssueAmountValueChange(e, "reward")
-            }
-          />
-        </RenderItemRow>
-        <RenderItemRow
-          label={t("bounty:service-fee.title")}
-          description={t("bounty:service-fee.support-message")}
-          handleLink={() => updateShowFeesModal(true)}
-        >
-          <InputNumber
-            symbol={currentToken?.symbol}
-            classSymbol=""
-            thousandSeparator
-            value={distributions?.totalServiceFees?.toFixed()}
-            disabled
-          />
-        </RenderItemRow>
-        <RenderItemRow
-          label={t("bounty:total-amount.title")}
-          description={t("bounty:total-amount.description")}
-          borderBottom={isFunding ? true : false}
-        >
-          {inputNumber()}
-        </RenderItemRow>
+        <AmountTokenInformation 
+          isFunding={isFunding} 
+          currentToken={currentToken} 
+          rewardAmount={rewardAmount} 
+          issueAmount={issueAmount} 
+          tokenBalance={tokenBalance} 
+          decimals={decimals} 
+          inputError={inputError} 
+          distributions={distributions} 
+          onIssueAmountValueChange={onIssueAmountValueChange}      
+          updateShowFeesModal={() => updateShowFeesModal(true)}
+        />
       </div>
     );
   }
