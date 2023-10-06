a0d72f91ba79d5edd41f9d28e1980aed9e5c360b
Marcus Vinícius <marcusvinici25@gmail.com>
2023-10-06 08:32:18 -0300

    removing use-get-profile-wallet

--- x-hooks/api/pages/profile/index.ts
+++ x-hooks/api/pages/profile/index.ts
@@ -1,3 +1,2 @@
 export * from "x-hooks/api/pages/profile/use-get-profile-bounties";
 export * from "x-hooks/api/pages/profile/use-get-profile-payments";
-export * from "x-hooks/api/pages/profile/use-get-profile-wallet";
