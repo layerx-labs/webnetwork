d6278c2d429312a17a54509b0846a8c75b80714b
vhcsilva <vhcsilvaa@gmail.com>
2023-10-12 19:08:17 -0300

    add more test cases

--- __tests__/components/connect-wallet-button/connect-wallet-button.view.test.tsx
+++ __tests__/components/connect-wallet-button/connect-wallet-button.view.test.tsx
@@ -6,26 +6,56 @@ import ConnectWalletButtonView from "components/connections/connect-wallet-butto
 
 import i18NextProviderTests from '__tests__/utils/i18next-provider';
 
+import "../styles/styles.scss";
+
 describe("ConnectWalletButtonView", () => {
-  const defaultProps = {
-    asModal: false,
-    isLoading: false,
-    isModalVisible: false,
-    hasWeb3Connection: false,
-    isConnected: false,
-    buttonColor: "white",
-    onConnectClick: jest.fn()
-  };
+  let defaultProps;
+
+  beforeEach(() => {
+    defaultProps = {
+      asModal: false,
+      isLoading: false,
+      isModalVisible: false,
+      hasWeb3Connection: false,
+      isConnected: false,
+      buttonColor: "white",
+      onConnectClick: jest.fn()
+    };
+  });
 
   it("Should render button if wallet is not connected", () => {
     const result = render(<ConnectWalletButtonView
                             {...defaultProps}
                           >
+                            <span data-testid="address">address</span>
                           </ConnectWalletButtonView>, {
                             wrapper: i18NextProviderTests
                           });
-    const button = result.queryByRole("button");
-    console.log(button.textContent)
-    expect(button).toBeVisible();
+    expect(result.queryByRole("button")).toBeVisible();
+    expect(result.queryByTestId("address")).toBeNull();
+  });
+
+  it("Should render children if wallet is connected", () => {
+    defaultProps.isConnected = true;
+    const result = render(<ConnectWalletButtonView
+                            {...defaultProps}
+                          >
+                            <span data-testid="address">address</span>
+                          </ConnectWalletButtonView>, {
+                            wrapper: i18NextProviderTests
+                          });
+    expect(result.queryByRole("button")).toBeNull();
+    expect(result.queryByTestId("address").textContent).toBe("address");
+  });
+
+  it("Should render modal if asModal is true and isLoading is false", () => {
+    defaultProps.asModal = true;
+    const result = render(<ConnectWalletButtonView
+                            {...defaultProps}
+                          >
+                            <span data-testid="address">address</span>
+                          </ConnectWalletButtonView>, {
+                            wrapper: i18NextProviderTests
+                          });
   });
 });
