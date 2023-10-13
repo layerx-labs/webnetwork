6edea7fec376e8ba8143d213ab7771b100162bb8
Marcus Vinícius <marcusvinici25@gmail.com>
2023-10-13 10:00:50 -0300

    adjusting fill-users-locked-registry migration

--- db/migrations/2023101111030000-fill-users-locked-registry-with-events.js
+++ db/migrations/2023101111030000-fill-users-locked-registry-with-events.js
@@ -2,8 +2,9 @@
 const {QueryTypes} = require("sequelize");
 
 const {Chain} = require("../models/chain.model");
+const {UserLockedRegistry} = require("../models/user-locked-registry");
 
-const {Web3Connection, NetworkRegistry} = require("@taikai/dappkit");
+const {Web3Connection, NetworkRegistry, fromSmartContractDecimals} = require("@taikai/dappkit");
 const BigNumber = require("bignumber.js");
 require("dotenv").config();
 
@@ -29,13 +30,13 @@ module.exports = {
       console.log("Chains to verify: ", chains.length);
   
       let UsersLockedRegistryUpdated = 0;
-  
+
       for (const chain of chains) {
         if (!chain.registryAddress) return;
   
         const web3Connection = new Web3Connection({
           skipWindowAssignment: true,
-          web3Host: process.env.NEXT_PUBLIC_WEB3_CONNECTION,
+          web3Host: chain.chainRpc,
         });
   
         await web3Connection.start();
@@ -43,6 +44,8 @@ module.exports = {
         const registry = new NetworkRegistry(web3Connection, chain.registryAddress);
         await registry.loadContract();
         await registry.start();
+
+        const contractToken = registry.token
   
         const blockNumber = await registry._contract.web3.eth.getBlockNumber();
   
@@ -55,7 +58,7 @@ module.exports = {
   
           let toBlock = 0;
   
-          console.log(`Fetching ${name} total of ${requests}, from: ${startBlock} to ${endBlock}`);
+          console.log(`Fetching ${name} total of ${requests}, from: ${startBlock} to ${endBlock} - chainId:${chain.chainId}`);
           for (let fromBlock = startBlock; fromBlock < endBlock; fromBlock += perRequest) {
             toBlock = fromBlock + perRequest > endBlock ? endBlock : fromBlock + perRequest;
   
@@ -75,10 +78,58 @@ module.exports = {
         const mappedUserLockedAmountChanged = getUserLockedAmountChangedEvents.map(({ returnValues }) => returnValues)
   
         for (const { user, newAmount } of mappedUserLockedAmountChanged) {
-  
-          const resultChangedEvent = 
-  
-          UsersLockedRegistryUpdated += resultChangedEvent ? 1 : 0;
+
+          const newUserAmount = BigNumber(fromSmartContractDecimals(newAmount || 0, registry.token.decimals));
+
+          const user_locked_registry = await queryInterface.sequelize.query(
+              'SELECT * FROM users_locked_registry WHERE LOWER(address) = LOWER(:address) AND "chainId" = :chainId',
+              {
+              replacements: {address: user, chainId: chain.chainId},
+              type: QueryTypes.SELECT,
+              }
+          );          
+          
+          if(user_locked_registry[0]){
+            const query = `UPDATE users_locked_registry SET "amountLocked" = $amountLocked WHERE id = $id`;
+
+            await queryInterface.sequelize.query(query, {
+                bind: {
+                  amountLocked: newUserAmount?.toFixed(),
+                  id: user_locked_registry[0].id,
+                },
+              });
+
+            UsersLockedRegistryUpdated += 1
+          } else {
+
+            const dbUser = await queryInterface.sequelize.query(
+                'SELECT * FROM users WHERE LOWER(address) = LOWER(:address)',
+                {
+                replacements: {address: user},
+                type: QueryTypes.SELECT,
+                }
+            ); 
+
+            const dbToken = await queryInterface.sequelize.query(
+                'SELECT * FROM tokens WHERE LOWER(address) = LOWER(:address) AND "chain_id" = :chain_id',
+                {
+                replacements: {address: contractToken.contractAddress?.toLowerCase(), chain_id: chain.chainId},
+                type: QueryTypes.SELECT,
+                }
+            ); 
+
+            await queryInterface.insert(UserLockedRegistry, "users_locked_registry", {
+                address: user,
+                userId: dbUser[0].id,
+                amountLocked: newUserAmount?.toFixed(),
+                chainId: chain.chainId,
+                tokenId: dbToken[0].id,
+                createdAt: new Date(),
+                updatedAt: new Date(),
+              });
+
+            UsersLockedRegistryUpdated += 1
+          }
   
           await sleep();
         }
