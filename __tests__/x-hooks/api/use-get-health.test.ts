434ccd459230b8c2165184cf769ee2ccfc7c4317
vhcsilva <vhcsilvaa@gmail.com>
2023-10-16 11:41:20 -0300

    more

--- __tests__/x-hooks/api/use-get-health.test.ts
+++ __tests__/x-hooks/api/use-get-health.test.ts
@@ -1,7 +1,16 @@
 import { useGetHealth } from "x-hooks/api/use-get-health";
 
-describe("UseGetHealth", () => {
-  it("Should return 200", async () => {
+const getMock = jest.fn();
+
+jest.mock("services/api", () => ({
+  api: {
+    get: async () => getMock()
+  }
+}));
+
+describe("useGetHealth", () => {
+  it("Should return 200 if server response is 200", async () => {
+    getMock.mockResolvedValue({ status: 200 })
     const response = await useGetHealth();
     expect(response.status).toBe(200);
   });
