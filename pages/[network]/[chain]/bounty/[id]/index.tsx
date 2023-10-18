42140dd8eebe97511d8229bb8badc904e8ed9ad5
vhcsilva <vhcsilvaa@gmail.com>
2023-10-18 11:48:25 -0300

    calculate developer amount

--- pages/[network]/[chain]/bounty/[id]/index.tsx
+++ pages/[network]/[chain]/bounty/[id]/index.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
 import { dehydrate } from "@tanstack/react-query";
+import BigNumber from "bignumber.js";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useRouter } from "next/router";
 import { GetServerSideProps } from "next/types";
@@ -17,6 +18,7 @@ import If from "components/If";
 import { useAppState } from "contexts/app-state";
 import { BountyEffectsProvider } from "contexts/bounty-effects";
 
+import { getDeveloperAmount } from "helpers/calculateDistributedAmounts";
 import { commentsParser, issueParser } from "helpers/issue";
 import { QueryKeys } from "helpers/query-keys";
 
@@ -42,7 +44,19 @@ export default function PageIssue() {
     useReactQuery(QueryKeys.bountyComments(bountyId?.toString()), () => 
       getCommentsData({ issueId: bountyId, type: "issue" }));
 
-  const parsedBounty = issueParser(bounty);
+  const treasury = state.Service?.network?.amounts?.treasury;
+  const mergeCreatorFeeShare = state.Service?.network?.amounts?.mergeCreatorFeeShare;
+  const proposerFeeShare = state.Service?.network?.amounts?.proposerFeeShare;
+
+  const developerAmount = getDeveloperAmount( treasury,
+                                              mergeCreatorFeeShare,
+                                              proposerFeeShare,
+                                              BigNumber(bounty?.amount));
+
+  const parsedBounty = issueParser({
+    ...bounty,
+    developerAmount
+  });
   const parsedComments = commentsParser(comments);
 
   const [isEditIssue, setIsEditIssue] = useState<boolean>(false);
