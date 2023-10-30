620093373edc1a5651aa8525d022362db1459868
Marcus Vinícius <marcusvinici25@gmail.com>
2023-10-30 11:48:24 -0300

    adjusting fill-price-tokens to find by address

--- helpers/fill-price-tokens-database.ts
+++ helpers/fill-price-tokens-database.ts
@@ -11,12 +11,32 @@ const {publicRuntimeConfig: {
 export default async function FillPriceTokensDatabase() {
   const currencys = currency || "usd";
 
-  const coins = await COINGECKO_API.get(`/coins/list?include_platform=false`).then((value) => value.data);
+  const coins = await COINGECKO_API.get(`/coins/list?include_platform=true`).then((value) => value.data);
 
   const tokens = await models.tokens.findAll({});
 
-  const coinsExistInDb = coins.filter((token) =>
-    [...tokens.map(({ symbol }) => symbol)].includes(token.symbol));
+  const dbSymbols = [...new Set(tokens.map(({ symbol }) => symbol?.toLowerCase()))]
+  const coinsBySymbolOnDb = coins.filter((token) => dbSymbols.includes(token.symbol?.toLowerCase()));
+
+  const coinsExistInDb = []
+ 
+  if(coinsBySymbolOnDb?.length !== dbSymbols?.length){
+    for(const coinBySymbolOnDb of coinsBySymbolOnDb){
+      const uniqueSymbol = coinsBySymbolOnDb.filter(v => v.symbol === coinBySymbolOnDb.symbol)?.length === 1
+      const platforms: { [key: string]: string } = coinBySymbolOnDb.platforms
+
+      if(!uniqueSymbol){
+        for (const [, value] of Object.entries(platforms)) { 
+          if(tokens.find(t => t?.address?.toLowerCase() === value?.toLowerCase()))
+            coinsExistInDb.push(coinBySymbolOnDb)
+        }
+      }
+      
+      if(uniqueSymbol) coinsExistInDb.push(coinBySymbolOnDb)
+    }
+  }
+
+  if(!coinsExistInDb?.length) throw new Error("coingecko did not find the id for the tokens");
 
   const ids = coinsExistInDb.map(({ id }) => id).join();
 
@@ -26,16 +46,36 @@ export default async function FillPriceTokensDatabase() {
     throw new Error("Error to get prices coingecko");
   }
 
-  const pricesBySymbol = coinsExistInDb.map(({ symbol, id }) =>
-      ({ [currencys]: price?.data?.[id]?.[currencys] }[symbol]));
+  const pricesBySymbol = coinsExistInDb.reduce((res, { symbol, id }) => {
+    res[symbol.toLowerCase()] = { [currencys]: price?.data?.[id]?.[currencys] };
+    return res;
+  }, {});
 
   for (const token of tokens) {
-    if (pricesBySymbol[token.symbol]) {
+    const coinsByTokenSymbol = coinsExistInDb.filter(v => v.symbol.toLowerCase() === token.symbol.toLowerCase())
+    const uniqueSymbol = coinsByTokenSymbol?.length === 1
+
+    if (pricesBySymbol[token.symbol.toLowerCase()] && uniqueSymbol) {
       token.last_price_used = {
-        ...pricesBySymbol[token.symbol],
+        ...pricesBySymbol[token.symbol.toLowerCase()],
         updatedAt: new Date(),
       };
       await token.save();
+    }
+
+    if(!uniqueSymbol) {
+      for(const ctoken of coinsExistInDb){
+        const platforms: { [key: string]: string } = ctoken.platforms
+        for(const [, value] of Object.entries(platforms)){
+          if(value.toLowerCase() === token.address.toLowerCase()){
+            token.last_price_used = {
+              [currencys]: price?.data?.[ctoken.id]?.[currencys],
+              updatedAt: new Date(),
+            }
+            await token.save();
+          }
+        }
+      }
     }
   }
 
