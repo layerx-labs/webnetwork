7f8467f56926c3cd72ce93a33ca51781b01556d9
MoshMage <moshmage@gmail.com>
2023-10-30 09:08:37 +0000

    make the check on the get-bounty-data.ts as well and return null.

--- x-hooks/api/bounty/get-bounty-data.ts
+++ x-hooks/api/bounty/get-bounty-data.ts
@@ -1,8 +1,8 @@
-import { ParsedUrlQuery } from "querystring";
+import {ParsedUrlQuery} from "querystring";
 
-import { IssueData } from "interfaces/issue-data";
+import {IssueData} from "interfaces/issue-data";
 
-import { api } from "services/api";
+import {api} from "services/api";
 
 /**
  * Get bounty from api based on query filters
@@ -11,6 +11,9 @@ import { api } from "services/api";
  */
 export async function getBountyData(query: ParsedUrlQuery): Promise<IssueData | null> {
   const { id, network: networkName, chain: chainName } = query;
+
+  if ([id, networkName, chainName].some(k => k === "undefined" || k === undefined))
+    return null;
 
   return api
     .get<IssueData>(`/issue/${id}/${networkName}/${chainName}`)
