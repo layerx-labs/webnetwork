d2a8693d340ed4a96e31e9f8d24305c565e1d3e9
Marcus Vinícius <marcusvinici25@gmail.com>
2023-10-31 16:14:09 -0300

    adjusting text useGetPrices

--- x-hooks/api/check-prices/use-get-prices.ts
+++ x-hooks/api/check-prices/use-get-prices.ts
@@ -7,8 +7,7 @@ interface tokens {
 
 /**
  * Get Tokens Prices from api
- * @param address and @param chainId
- * @returns list of filtered chains
+ * @returns prices
  */
 export async function useGetPrices(tokens: tokens[]) {
   return api.post("/check-prices", { tokens }).then(({ data }) => data);
