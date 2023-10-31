aa5e4ff0ab43de71ce68e74ade8ac431121f2b11
vhcsilva <vhcsilvaa@gmail.com>
2023-10-31 11:07:00 -0300

    add tokens to avoid filtering when getting tokens

--- x-hooks/stores/marketplace/use-marketplace.store.ts
+++ x-hooks/stores/marketplace/use-marketplace.store.ts
@@ -2,12 +2,15 @@ import { create } from "zustand";
 
 import { Network } from "interfaces/network";
 import { SupportedChainData } from "interfaces/supported-chain-data";
+import { Token } from "interfaces/token";
 
 interface ActiveMarketplace {
   lastVisited: string | null;
   active: Network | null;
   noDefaultNetwork: boolean | null;
   availableChains: SupportedChainData[] | null;
+  transactionalTokens: Token[] | null;
+  rewardTokens: Token[] | null;
 }
 
 type UseMarketplaceStore = {
@@ -17,6 +20,8 @@ type UseMarketplaceStore = {
   updateActive: (active: Network) => ActiveMarketplace,
   updateNoDefaultNetwork: (noDefaultNetwork: boolean) => ActiveMarketplace,
   updateAvailableChains: (availableChains: SupportedChainData[]) => ActiveMarketplace,
+  updateTransactionalTokens: (transactionalTokens: Token[]) => ActiveMarketplace,
+  updateRewardTokens: (rewardTokens: Token[]) => ActiveMarketplace,
 }
 
 export const useMarketplaceStore = create<UseMarketplaceStore>((set, get) => {
@@ -34,12 +39,16 @@ export const useMarketplaceStore = create<UseMarketplaceStore>((set, get) => {
       active: null,
       noDefaultNetwork: null,
       availableChains: null,
+      transactionalTokens: null,
+      rewardTokens: null
     },
     update,
     updateLastVisited: (lastVisited: string) => update({ lastVisited }),
     updateActive: (active: Network) => update({ active }),
     updateNoDefaultNetwork: (noDefaultNetwork: boolean) => update({ noDefaultNetwork }),
     updateAvailableChains: (availableChains: SupportedChainData[]) => update({ availableChains }),
+    updateTransactionalTokens: (transactionalTokens: Token[]) => update({ transactionalTokens }),
+    updateRewardTokens: (rewardTokens: Token[]) => update({ rewardTokens }),
   }
 });
 
