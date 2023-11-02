443e68820638db47b18dbbb3d35e8c833f36ba3a
vhcsilva <vhcsilvaa@gmail.com>
2023-11-02 14:49:42 -0300

    add clear method

--- x-hooks/stores/marketplace/use-marketplace.store.ts
+++ x-hooks/stores/marketplace/use-marketplace.store.ts
@@ -7,7 +7,6 @@ import { Token } from "interfaces/token";
 interface ActiveMarketplace {
   lastVisited: string | null;
   active: Network | null;
-  noDefaultNetwork: boolean | null;
   availableChains: SupportedChainData[] | null;
   transactionalTokens: Token[] | null;
   rewardTokens: Token[] | null;
@@ -18,13 +17,21 @@ type UseMarketplaceStore = {
   update: (data: Partial<ActiveMarketplace>) => ActiveMarketplace,
   updateLastVisited: (lastVisited: string) => ActiveMarketplace,
   updateActive: (active: Network) => ActiveMarketplace,
-  updateNoDefaultNetwork: (noDefaultNetwork: boolean) => ActiveMarketplace,
   updateAvailableChains: (availableChains: SupportedChainData[]) => ActiveMarketplace,
   updateTransactionalTokens: (transactionalTokens: Token[]) => ActiveMarketplace,
   updateRewardTokens: (rewardTokens: Token[]) => ActiveMarketplace,
+  clear: () => void,
 }
 
 export const useMarketplaceStore = create<UseMarketplaceStore>((set, get) => {
+  const cleanData = {
+    lastVisited: null,
+    active: null,
+    availableChains: null,
+    transactionalTokens: null,
+    rewardTokens: null
+  };
+
   const update = (data: Partial<ActiveMarketplace>) => {
     const updated = Object.assign({}, get().data, data);
     set(() => ({
@@ -34,21 +41,14 @@ export const useMarketplaceStore = create<UseMarketplaceStore>((set, get) => {
   };
 
   return {
-    data: {
-      lastVisited: null,
-      active: null,
-      noDefaultNetwork: null,
-      availableChains: null,
-      transactionalTokens: null,
-      rewardTokens: null
-    },
+    data: cleanData,
     update,
     updateLastVisited: (lastVisited: string) => update({ lastVisited }),
     updateActive: (active: Network) => update({ active }),
-    updateNoDefaultNetwork: (noDefaultNetwork: boolean) => update({ noDefaultNetwork }),
     updateAvailableChains: (availableChains: SupportedChainData[]) => update({ availableChains }),
     updateTransactionalTokens: (transactionalTokens: Token[]) => update({ transactionalTokens }),
     updateRewardTokens: (rewardTokens: Token[]) => update({ rewardTokens }),
+    clear: () => update(cleanData),
   }
 });
 
