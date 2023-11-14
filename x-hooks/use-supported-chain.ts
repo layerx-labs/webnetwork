1eea66bb85f6b1afc1837a407457361837326347
Marcus Vinícius <marcusvinici25@gmail.com>
2023-11-14 12:09:51 -0300

    adding useSupportedChain

---
+++ x-hooks/use-supported-chain.ts
@@ -0,0 +1,61 @@
+import { useEffect } from "react";
+
+import { useRouter } from "next/router";
+
+import { useAppState } from "contexts/app-state";
+
+import { MINUTE_IN_MS } from "helpers/constants";
+import { QueryKeys } from "helpers/query-keys";
+
+import useReactQuery from "x-hooks/use-react-query";
+
+import { useGetChains } from "./api/chain";
+import { useSupportedChainStore } from "./stores/supported-chain/supported-chain.store";
+
+export default function useSupportedChain() {
+  const { query } = useRouter();
+
+  const { state } = useAppState();
+  const {
+    chains: supportedChains,
+    connectedChain,
+    updateChains,
+    updateConnectedChain,
+  } = useSupportedChainStore();
+
+  const { invalidate } = useReactQuery(QueryKeys.chains(),
+                                       () =>
+      useGetChains().then((chains) => {
+        updateChains(chains);
+        return chains;
+      }),
+                                       {
+      staleTime: MINUTE_IN_MS,
+                                       });
+
+  function updateNetworkAndChainMatch() {
+    const connectedChainId = connectedChain?.id;
+    const networkChainId = state?.Service?.network?.active?.chain_id;
+    const isOnANetwork = !!query?.network;
+
+    if (connectedChainId && networkChainId && isOnANetwork)
+      updateConnectedChain({
+        ...connectedChain,
+        matchWithNetworkChain: +connectedChainId === +networkChainId,
+      });
+    else updateConnectedChain(null);
+  }
+
+  useEffect(updateNetworkAndChainMatch, [
+    query?.network,
+    query?.chain,
+    connectedChain?.id,
+    state.Service?.network?.active?.chain_id,
+  ]);
+
+  return {
+    supportedChains,
+    connectedChain,
+    refresh: invalidate,
+  };
+}
