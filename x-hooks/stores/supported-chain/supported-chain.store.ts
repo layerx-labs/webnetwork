d0225a10e5b2eba05467939a6279fe445e7a04f1
Marcus Vinícius <marcusvinici25@gmail.com>
2023-11-15 11:54:14 -0300

    adding logic to getChainsDatabase

--- x-hooks/stores/supported-chain/supported-chain.store.ts
+++ x-hooks/stores/supported-chain/supported-chain.store.ts
@@ -6,16 +6,20 @@ import { SupportedChainData } from "interfaces/supported-chain-data";
 type useSupportedChainStore = {
   chains: SupportedChainData[];
   connectedChain: ConnectedChain;
+  isGetChainsDatabase: boolean;
   updateChains: (chains: SupportedChainData[]) => SupportedChainData[];
   updateConnectedChain: (chain: ConnectedChain) => ConnectedChain;
+  loadChainsDatabase: () => void
 };
 
 export const useSupportedChainStore = create<useSupportedChainStore>((set, get) => ({
     chains: [],
     connectedChain: null,
+    isGetChainsDatabase: false,
     updateChains: (chains: SupportedChainData[]) => {
       set(() => ({
         chains,
+        isGetChainsDatabase: get().isGetChainsDatabase,
         connectedChain: get().connectedChain,
       }));
       return chains;
@@ -23,8 +27,16 @@ export const useSupportedChainStore = create<useSupportedChainStore>((set, get) 
     updateConnectedChain: (connectedChain: ConnectedChain) => {
       set(() => ({
         chains: get().chains,
+        isGetChainsDatabase: get().isGetChainsDatabase,
         connectedChain,
       }));
       return connectedChain;
     },
+    loadChainsDatabase: () => {
+      set(() => ({
+        chains: get().chains,
+        isGetChainsDatabase: true,
+        connectedChain: get().connectedChain,
+      }));
+    }
 }));
