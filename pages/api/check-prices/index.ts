57f5fed43bd8aa8028a35862a97aa5bd26e1ad4a
vhcsilva <vhcsilvaa@gmail.com>
2023-11-17 09:58:47 -0300

    refactor to send response on endpoint function

--- pages/api/check-prices/index.ts
+++ pages/api/check-prices/index.ts
@@ -2,16 +2,23 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 import { withCORS } from "middleware";
 
+import {error as LogError} from "services/logging";
+
 import post from "server/common/check-prices/post";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  switch (req.method) {
-  case "POST":
-    await post(req, res);
-    break;
+  try {
+    switch (req.method) {
+    case "POST":
+      res.status(200).json(await post(req));
+      break;
 
-  default:
-    res.status(405);
+    default:
+      res.status(405);
+    }
+  } catch (error) {
+    LogError(error);
+    res.status(error?.status || 500).json(error?.message || error?.toString());
   }
 
   res.end();
