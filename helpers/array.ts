98baaad234ed283cd86e0008ba60561a5c51c3d2
vhcsilva <vhcsilvaa@gmail.com>
2023-11-17 16:39:14 -0300

    add curator overview endpoint

--- helpers/array.ts
+++ helpers/array.ts
@@ -4,10 +4,10 @@ export const isSameSet = (s1, s2) => {
   return isContain(s1, s2) && isContain(s2, s1);
 };
 
-export const orderByProperty = (elements, property, order) => {
+export const orderByProperty = (elements, property, order = "desc", ) => {
   return elements.sort((elA, elB) => {
-    if (elA[property] < elB[property]) return -1 * (order === "desc" ? -1 : 1);
-    if (elA[property] > elB[property]) return 1 * (order === "desc" ? -1 : 1);
+    if (elA[property] < elB[property]) return -1 * (order.toLowerCase() === "desc" ? -1 : 1);
+    if (elA[property] > elB[property]) return 1 * (order.toLowerCase() === "desc" ? -1 : 1);
 
     return 0;
   });
