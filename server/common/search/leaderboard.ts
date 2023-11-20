5733a535a02b1ac517968590fa40660a7db3b439
Marcus Vinícius <marcusvinici25@gmail.com>
2023-11-20 18:12:46 -0300

    adding processLeaderBoard

--- server/common/search/leaderboard.ts
+++ server/common/search/leaderboard.ts
@@ -3,7 +3,37 @@ import { Op, Sequelize, WhereOptions } from "sequelize";
 
 import models from "db/models";
 
+import { caseInsensitiveEqual } from "helpers/db/conditionals";
 import paginate, { DEFAULT_ITEMS_PER_PAGE, calculateTotalPages } from "helpers/paginate";
+
+function processLeaderboard(leaderboards) {
+  const users: {
+    [address: string]: {
+      networks: string[],
+      chains: string[]
+    } 
+  } = {}
+
+  leaderboards?.forEach((entry) => {
+    const address = entry?.address;
+    const networkIcon = entry?.userPayments?.issue?.network?.logoIcon;
+    const alreadyExistNetworkIcon = users[address]?.networks?.find(v => v === networkIcon);
+    const chainIcon = entry?.userPayments?.issue?.network?.chain?.icon;
+    const alreadyExistChainIcon = users[address]?.chains?.find(v => v === chainIcon);
+
+    if (!users[address]) users[address] = { networks: [], chains: [] };
+
+    if (networkIcon && !alreadyExistNetworkIcon) {
+      users[address]?.networks?.push(networkIcon);
+    }
+
+    if(chainIcon && !alreadyExistChainIcon) {
+      users[address]?.chains?.push(chainIcon);
+    }
+  });
+
+  return users;
+}
 
 export default async function get(query: ParsedUrlQuery) {
   const {
@@ -11,7 +41,8 @@ export default async function get(query: ParsedUrlQuery) {
     page,
     search,
     sortBy,
-    order
+    order,
+    networkName
   } = query;
 
   const whereCondition: WhereOptions = {};
@@ -28,22 +59,60 @@ export default async function get(query: ParsedUrlQuery) {
   const PAGE = +(page || 1);
 
   const leaderboard = await models.leaderBoard.findAndCountAll(paginate({
-    attributes: {
-      exclude: ["id"]
-    },
-    where: whereCondition,
-    include: [
-      {
-        association: "user",
-        attributes: ["githubLogin"],
-        required: !!search,
-        on: Sequelize.where(Sequelize.fn("lower", Sequelize.col("user.address")),
-                            "=",
-                            Sequelize.fn("lower", Sequelize.col("leaderboard.address"))),
-        where: whereInclude
-      }
-    ]
-  }, { page: PAGE },  [[sortBy || "numberNfts", order || "DESC"]]));
+        attributes: {
+          exclude: ["id"],
+        },
+        where: whereCondition,
+        subQuery: false,
+        include: [
+          {
+            association: "user",
+            attributes: ["githubLogin"],
+            required: !!search,
+            on: Sequelize.where(Sequelize.fn("lower", Sequelize.col("user.address")),
+                                "=",
+                                Sequelize.fn("lower", Sequelize.col("leaderboard.address"))),
+            where: whereInclude,
+          },
+          {
+            association: "userPayments",
+            required: networkName === 'all' || !networkName ? false : true,
+            on: Sequelize.where(Sequelize.fn("lower", Sequelize.col("userPayments.address")),
+                                "=",
+                                Sequelize.fn("lower", Sequelize.col("leaderboard.address"))),
+            include: [
+              {
+                association: "issue",
+                required: true,
+                include: [
+                  {
+                    association: "network",
+                    required: !!networkName,
+                    include: [{ association: "chain", attributes: ["icon"] }],
+                    where:
+                      networkName === "all"
+                        ? {}
+                        : networkName
+                        ? {
+                            name: caseInsensitiveEqual("name",
+                                                       networkName?.toString()),
+                        }
+                        : {},
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+  },
+      { page: PAGE },
+      [[sortBy || "numberNfts", order || "DESC"]])).then((res) => {
+
+        const leaderBoardData = processLeaderboard(res?.rows)
+        console.log('processLeaderboard', leaderBoardData)
+        
+        return res
+      })
 
   return {
     ...leaderboard,
