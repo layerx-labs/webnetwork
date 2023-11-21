2932de0a224cb1f84f481d9526a16aef9f657b5c
Marcus Vinícius <marcusvinici25@gmail.com>
2023-11-21 18:50:07 -0300

    refactoring logic search/leaderboard

--- server/common/search/leaderboard.ts
+++ server/common/search/leaderboard.ts
@@ -1,122 +1,83 @@
 import { ParsedUrlQuery } from "querystring";
-import { Op, Sequelize, WhereOptions } from "sequelize";
+import { WhereOptions } from "sequelize";
 
 import models from "db/models";
+import Database from "db/models";
 
-import { caseInsensitiveEqual } from "helpers/db/conditionals";
-import paginate, { DEFAULT_ITEMS_PER_PAGE, calculateTotalPages } from "helpers/paginate";
-
-function processLeaderboard(leaderboards) {
-  const users: {
-    [address: string]: {
-      networks: string[],
-      chains: string[]
-    } 
-  } = {}
-
-  leaderboards?.forEach((entry) => {
-    const address = entry?.address;
-    const networkIcon = entry?.userPayments?.issue?.network?.logoIcon;
-    const alreadyExistNetworkIcon = users[address]?.networks?.find(v => v === networkIcon);
-    const chainIcon = entry?.userPayments?.issue?.network?.chain?.icon;
-    const alreadyExistChainIcon = users[address]?.chains?.find(v => v === chainIcon);
-
-    if (!users[address]) users[address] = { networks: [], chains: [] };
-
-    if (networkIcon && !alreadyExistNetworkIcon) {
-      users[address]?.networks?.push(networkIcon);
-    }
-
-    if(chainIcon && !alreadyExistChainIcon) {
-      users[address]?.chains?.push(chainIcon);
-    }
-  });
-
-  return users;
-}
+import { DEFAULT_ITEMS_PER_PAGE, calculateTotalPages } from "helpers/paginate";
 
 export default async function get(query: ParsedUrlQuery) {
-  const {
-    address,
-    page,
-    search,
-    sortBy,
-    order,
-    networkName
-  } = query;
+  const { address, page, search, sortBy, order, networkName } = query;
 
   const whereCondition: WhereOptions = {};
 
   if (address) whereCondition.address = address;
 
-  const whereInclude = search ? {
-    [Op.or]: [
-      { address: { [Op.iLike]: `%${search}%` } },
-      { githubLogin: { [Op.iLike]: `%${search}%` } },
-    ]
-  }  : {};
-  
   const PAGE = +(page || 1);
+  const OFFSET = (PAGE - 1) * DEFAULT_ITEMS_PER_PAGE;
 
-  const leaderboard = await models.leaderBoard.findAndCountAll(paginate({
-        attributes: {
-          exclude: ["id"],
-        },
-        where: whereCondition,
-        subQuery: false,
-        include: [
-          {
-            association: "user",
-            attributes: ["githubLogin"],
-            required: !!search,
-            on: Sequelize.where(Sequelize.fn("lower", Sequelize.col("user.address")),
-                                "=",
-                                Sequelize.fn("lower", Sequelize.col("leaderboard.address"))),
-            where: whereInclude,
-          },
-          {
-            association: "userPayments",
-            required: networkName === 'all' || !networkName ? false : true,
-            on: Sequelize.where(Sequelize.fn("lower", Sequelize.col("userPayments.address")),
-                                "=",
-                                Sequelize.fn("lower", Sequelize.col("leaderboard.address"))),
-            include: [
-              {
-                association: "issue",
-                required: true,
-                include: [
-                  {
-                    association: "network",
-                    required: !!networkName,
-                    include: [{ association: "chain", attributes: ["icon"] }],
-                    where:
-                      networkName === "all"
-                        ? {}
-                        : networkName
-                        ? {
-                            name: caseInsensitiveEqual("name",
-                                                       networkName?.toString()),
-                        }
-                        : {},
-                  },
-                ],
-              },
-            ],
-          },
-        ],
-  },
-      { page: PAGE },
-      [[sortBy || "numberNfts", order || "DESC"]])).then((res) => {
+  const leaderboardQuery = `
+    SELECT
+      address, 
+      githubLogin,
+      COUNT(issue) AS numberNfts,
+      array_to_string(array_agg(DISTINCT "logoIcon"), ', ') AS networksLogos,
+      array_to_string(array_agg(DISTINCT icon), ', ') AS chainsLogos
+    FROM (
+      SELECT 
+        up.address,
+        u."githubLogin" AS githubLogin,
+        i.id AS issue, 
+        n."logoIcon", 
+        c.icon
+      FROM 
+        users_payments up
+        INNER JOIN users u ON LOWER(u.address) = LOWER(up.address)
+        INNER JOIN issues i ON i.id = up."issueId"
+        INNER JOIN networks n ON n.id = i.network_id
+        INNER JOIN chains c ON c."chainId" = i.chain_id
+      WHERE 1 = 1
+      ${
+        search
+          ? `AND (
+              u.address ILIKE '%${search}%' OR
+              u."githubLogin" ILIKE '%${search}%'
+            )`
+          : ""
+      }
+      ${networkName === 'all' ? '' : networkName ? `AND n.name = LOWER('${networkName}')` : ""}
+      ${address  ? `AND u.address = LOWER('${address}')` : ""}
+    ) tbl
+    GROUP BY address, githubLogin
+    ORDER BY COUNT(issue) ${order ? order : "DESC"}
+    OFFSET ${OFFSET} LIMIT ${DEFAULT_ITEMS_PER_PAGE};
+`;
 
-        const leaderBoardData = processLeaderboard(res?.rows)
-        console.log('processLeaderboard', leaderBoardData)
-        
-        return res
-      })
+  const countLeaderboard = await models.userPayments.count({
+    distinct: true,
+    col: "address",
+  });
+
+  const leaderboard = await Database.sequelize
+    .query(leaderboardQuery, {
+      type: Database.sequelize.QueryTypes.SELECT,
+    })
+    .then((leaderboards) => {
+      return {
+        count: countLeaderboard,
+        rows: leaderboards.map((l) => ({
+          ...l,
+          ... l?.githublogin ? {user: { githubLogin: l?.githublogin }} : null,
+          numberNfts: l?.numbernfts,
+          chainslogos: l?.chainslogos?.split(", "),
+          networkslogos: l?.networkslogos?.split(", "),
+        })),
+      };
+    });
 
   return {
     ...leaderboard,
     currentPage: PAGE,
-    pages: calculateTotalPages(leaderboard.count, DEFAULT_ITEMS_PER_PAGE)
+    pages: calculateTotalPages(countLeaderboard, DEFAULT_ITEMS_PER_PAGE),
   };
 }
