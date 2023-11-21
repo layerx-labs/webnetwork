06dd24f27318c5c6efe05a3127dd30ef9ff3b5ec
vhcsilva <vhcsilvaa@gmail.com>
2023-11-21 10:14:00 -0300

    add get total users api hook

--- x-hooks/api/user/index.ts
+++ x-hooks/api/user/index.ts
@@ -2,3 +2,4 @@ export * from "x-hooks/api/user/use-get-user-by-address";
 export * from "x-hooks/api/user/use-get-user-by-login";
 export * from "x-hooks/api/user/use-update-email";
 export * from "x-hooks/api/user/use-remove-github";
+export * from "x-hooks/api/user/use-get-total-users";
