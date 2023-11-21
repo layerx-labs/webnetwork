48b4b255889ad0b229e1085acf9561813631a892
Marcus Vinícius <marcusvinici25@gmail.com>
2023-11-21 10:38:45 -0300

    Revert "adding avatar space"
    This reverts commit 719d74bbfd6dcf99461b8abad5494d510b107c53.

--- components/bounty/bounty-hero/view.tsx
+++ components/bounty/bounty-hero/view.tsx
@@ -178,7 +178,7 @@ export default function BountyHeroView({
               <div className="d-flex align-items-center">
                 <BountyItemLabel label={t("common:misc.owner")} className="col-12 col-sm-auto">
                   <>
-                    <div className="d-flex flex-column justify-content-center me-1">
+                    <div className="d-flex flex-column justify-content-center">
                       <Avatar
                         size="xsm"
                         userLogin={bounty?.user?.githubLogin}
