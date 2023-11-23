e7d8b351108f97cd7429b9dce29b206ce46b14dd
MoshMage <moshmage@gmail.com>
2023-11-23 13:30:27 +0000

    update migration to update the userId value for each curator based on the user with the same address

--- db/migrations/2023231111150000-add-users-curator-relation.js
+++ db/migrations/2023231111150000-add-users-curator-relation.js
@@ -1,3 +1,5 @@
+const { QueryTypes } = require("sequelize");
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.addColumn('curators', 'userId', {
@@ -9,6 +11,11 @@ module.exports = {
       onUpdate: 'CASCADE',
       onDelete: 'SET NULL',
     });
+
+    await queryInterface.sequelize.query(
+      `UPDATE "curators" AS c SET "userId" = u."id" FROM "users" AS u WHERE c."address" = u."address";`,
+      {type: QueryTypes.UPDATE}
+    )
   },
 
   down: async (queryInterface, Sequelize) => {
