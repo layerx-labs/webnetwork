c6bef550463fa0a0ff5fd1a3c57af10962b3cfdc
Marcus Vinícius <marcusvinici25@gmail.com>
2023-11-23 18:29:36 -0300

    adding useSettingsStore to use-settings hook

--- x-hooks/use-settings.ts
+++ x-hooks/use-settings.ts
@@ -1,50 +1,44 @@
-import {useState} from "react";
+import { useState } from "react";
 
-import {useAppState} from "../contexts/app-state";
-import {updateSettings} from "../contexts/reducers/change-settings";
-import {updateShowProp} from "../contexts/reducers/update-show-prop";
-import {WinStorage} from "../services/win-storage";
+import { WinStorage } from "../services/win-storage";
 import { useGetSettings } from "./api/use-get-settings";
+import { useSettingsStore } from "./stores/settings/settings.store";
 
 /**
  * Loads settings with useEffect if not loaded previously
  */
 export function useSettings() {
-  const {state, dispatch} = useAppState();
+  const { data: Settings, updateSettings } = useSettingsStore();
 
-  const [storage,] =
-    useState<WinStorage>(new WinStorage('web-network.settings', 3600 * 1000, 'sessionStorage'))
+  const [storage] = useState<WinStorage>(new WinStorage("web-network.settings", 3600 * 1000, "sessionStorage"));
 
   /**
-   * Load settings on useSettings() start only if `state.Settings` is empty
+   * Load settings on useSettings() start only if `Settings` is empty
    * Reload settings on each session start
    */
   function loadSettings(force?: boolean) {
-    if (state.Settings && !force)
-      return;
+    if (Settings && !force) return;
 
     if (storage.value && !force) {
-      dispatch(updateSettings(storage.value));
+      updateSettings(storage.value);
       // return storage.value;
       return;
     }
 
-    dispatch(updateShowProp({failedLoadSettings: false}));
-    dispatch(updateSettings({} as any));
+    updateSettings(null);
     useGetSettings()
-      .then(settings => {
+      .then((settings) => {
         storage.value = settings;
         // setTmpSettings(settings)
-        dispatch(updateSettings(settings));
+        updateSettings(settings);
         // return settings;
       })
-      .catch(e => {
+      .catch((e) => {
         console.error(`Failed to load settings from db`, e);
-        dispatch(updateShowProp({failedLoadSettings: true}));
       });
   }
 
   return {
-    loadSettings
-  }
+    loadSettings,
+  };
 }
