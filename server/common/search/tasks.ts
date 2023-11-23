cf93bdd2e07983d470c67bcccc9ccd2f2f03de17
Marcus Vinícius <marcusvinici25@gmail.com>
2023-11-23 17:40:18 -0300

    adding push is closed (#283)

--- server/common/search/tasks.ts
+++ server/common/search/tasks.ts
@@ -40,6 +40,9 @@ export default async function get(query: ParsedUrlQuery) {
   if (["disputable", "mergeable", "proposable"].includes(state?.toString()))
     defaultStatesToIgnore.push("closed", "draft");
 
+  if (!state && !chain && !search)
+    defaultStatesToIgnore.push("closed");
+
   if (!creator)
     // Issue table columns
     whereCondition.state = {
