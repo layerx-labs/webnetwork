4e21facab000aab5c78abfb200a0c95857357bcf
Marcus Vinícius <marcusvinici25@gmail.com>
2023-11-24 13:50:39 -0300

    adding logic to use react-query in settings

--- x-hooks/stores/settings/settings.store.ts
+++ x-hooks/stores/settings/settings.store.ts
@@ -4,14 +4,24 @@ import { SettingsType } from "types/settings";
 
 type SettingsStore = {
   data: SettingsType | null;
+  isGetSettingsDatabase: boolean;
   updateSettings: (settings: SettingsType) => void;
+  loadSettingsDatabase: () => void
 };
 
-export const useSettingsStore = create<SettingsStore>((set) => ({
+export const useSettingsStore = create<SettingsStore>((set, get) => ({
   data: null,
+  isGetSettingsDatabase: false,
   updateSettings: (settings: SettingsType) => {
     set(() => ({
       data: JSON.parse(JSON.stringify(settings || {})),
+      isGetSettingsDatabase: false
     }));
   },
+  loadSettingsDatabase: () => {
+    set(() => ({
+      data: get().data,
+      isGetSettingsDatabase: true
+    }));
+  }
 }));
--- x-hooks/use-settings.ts
+++ x-hooks/use-settings.ts
@@ -1,16 +1,44 @@
 import { useState } from "react";
 
+import { MINUTE_IN_MS } from "helpers/constants";
+import { QueryKeys } from "helpers/query-keys";
+
 import { WinStorage } from "../services/win-storage";
 import { useGetSettings } from "./api/use-get-settings";
 import { useSettingsStore } from "./stores/settings/settings.store";
+import useReactQuery from "./use-react-query";
 
 /**
  * Loads settings with useEffect if not loaded previously
  */
 export function useSettings() {
-  const { data: Settings, updateSettings } = useSettingsStore();
+  const {
+    data: Settings,
+    updateSettings,
+    loadSettingsDatabase,
+    isGetSettingsDatabase,
+  } = useSettingsStore();
 
   const [storage] = useState<WinStorage>(new WinStorage("web-network.settings", 3600 * 1000, "sessionStorage"));
+
+  const { invalidate } = useReactQuery(QueryKeys.settings(),
+                                       () =>
+      useGetSettings()
+        .then((settings) => {
+          storage.value = settings;
+          // setTmpSettings(settings)
+          updateSettings(settings);
+          return settings;
+        })
+        .catch((e) => {
+          console.error(`Failed to load settings from db`, e);
+          updateSettings(null);
+          return null;
+        }),
+                                       {
+      staleTime: MINUTE_IN_MS,
+      enabled: isGetSettingsDatabase,
+                                       });
 
   /**
    * Load settings on useSettings() start only if `Settings` is empty
@@ -26,19 +54,12 @@ export function useSettings() {
     }
 
     updateSettings(null);
-    useGetSettings()
-      .then((settings) => {
-        storage.value = settings;
-        // setTmpSettings(settings)
-        updateSettings(settings);
-        // return settings;
-      })
-      .catch((e) => {
-        console.error(`Failed to load settings from db`, e);
-      });
+    loadSettingsDatabase();
   }
 
   return {
+    Settings,
+    refresh: invalidate,
     loadSettings,
   };
 }
