3b4358977b0e713ab47c3ce1b5c83b1d91b9bb50
Marcus Vinícius <marcusvinici25@gmail.com>
2023-11-27 18:08:18 -0300

    removing imports github-provider

--- server/auth/providers/index.ts
+++ server/auth/providers/index.ts
@@ -2,7 +2,6 @@ import { CallbacksOptions } from "next-auth";
 import { Provider } from "next-auth/providers";
 
 export * from "server/auth/providers/ethereum-provider";
-export * from "server/auth/providers/github-provider";
 
 export interface AuthProvider {
   config: Provider;
