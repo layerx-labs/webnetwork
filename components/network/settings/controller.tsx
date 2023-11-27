8fa1693cb8df158a97b7a6dd43b8155c520a83e8
vhcsilva <vhcsilvaa@gmail.com>
2023-11-27 18:48:28 -0300

    use role to check if is registry governor

--- components/network/settings/controller.tsx
+++ components/network/settings/controller.tsx
@@ -47,19 +47,18 @@ export default function MyNetworkSettings({
 }: MyNetworkSettingsProps) {
   const { t } = useTranslation(["common", "custom-network", "bounty"]);
 
-  const [errorBigImages, setErrorBigImages] = useState(false);
-  const [isGovernorRegistry, setIsGovernorRegistry] = useState(false);
   const [tabs, setTabs] = useState<TabsProps[]>([]);
+  const [errorBigImages, setErrorBigImages] = useState(false);
   const [activeTab, setActiveTab] = useState("logo-and-colours");
 
   const { currentUser } = useUserStore();
   const marketplace = useMarketplace();
-  const { isRegistryGovernor } = useBepro();
   const { colorsToCSS } = useNetworkTheme();
   const { service: daoService } = useDaoStore();
   const { connectedChain } = useSupportedChain();
   const { details, settings, forcedNetwork, clearState } = useNetworkSettings();
 
+  const isGovernorRegistry = state?.currentUser?.isAdmin;
   const chainId = connectedChain?.id;
   const { mutate: updateNetwork, isLoading: isUpdating } = useReactQueryMutation({
     queryKey: QueryKeys.networksByGovernor(currentUser?.walletAddress, chainId),
@@ -126,15 +125,6 @@ export default function MyNetworkSettings({
       setErrorBigImages(false);
     }
   }, [details?.fullLogo, details?.iconLogo]);
-
-  useEffect(() => {
-    if (!daoService?.registry?.contractAddress ||
-        !currentUser?.walletAddress ||
-        !connectedChain?.id) return;
-
-    isRegistryGovernor(currentUser?.walletAddress)
-      .then(setIsGovernorRegistry);
-  }, [currentUser?.walletAddress, daoService?.registry?.contractAddress, connectedChain?.id]);
 
   useEffect(() => {
     if(!network) return;
