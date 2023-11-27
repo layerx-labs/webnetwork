2ddf7526c7090d0abab5a1640bbd310f9094cfac
Marcus Vinícius <marcusvinici25@gmail.com>
2023-11-27 11:36:58 -0300

    removing global-effects and adjusting rootProvider

--- contexts/index.tsx
+++ contexts/index.tsx
@@ -5,32 +5,61 @@ import { useSession } from "next-auth/react";
 import NetworkThemeInjector from "components/custom-network/network-theme-injector";
 
 import { useDaoStore } from "x-hooks/stores/dao/dao.store";
+import { useUserStore } from "x-hooks/stores/user/user.store";
 import { useAuthentication } from "x-hooks/use-authentication";
+import { useDao } from "x-hooks/use-dao";
+import useMarketplace from "x-hooks/use-marketplace";
 import { useSettings } from "x-hooks/use-settings";
-
-import { GlobalEffectsProvider } from "./global-effects";
+import useSupportedChain from "x-hooks/use-supported-chain";
 
 const RootProviders = ({ children }) => {
   const session = useSession();
   const { loadSettings } = useSettings();
   const { service: daoService } = useDaoStore();
-  const { syncUserDataWithSession } = useAuthentication();
+  const { currentUser } = useUserStore();
+  const { supportedChains, connectedChain } = useSupportedChain();
+  const marketplace = useMarketplace();
+  const { listenChainChanged, changeNetwork, start: daoServiceStart } = useDao();
+  const { syncUserDataWithSession, updateWalletBalance, verifyReAuthorizationNeed } = useAuthentication();
   
   useEffect(() => {
     session.update();
+    loadSettings()
   }, []);
-
-  useEffect(loadSettings, []);
 
   useEffect(() => {
     syncUserDataWithSession();
   }, [daoService, session]);
 
+  useEffect(listenChainChanged, [ supportedChains ]);
+
+  useEffect(() => {
+    daoServiceStart()
+  }, [
+    marketplace?.active?.chain_id,
+    connectedChain?.id,
+    connectedChain?.registry,
+    currentUser?.connected
+  ]);
+
+  useEffect(() => {
+    changeNetwork();
+  }, [
+    daoService,
+    marketplace?.active?.networkAddress,
+    marketplace?.active?.chain_id,
+  ]);
+
+  
+  useEffect(updateWalletBalance, [currentUser?.walletAddress, daoService?.network?.contractAddress]);
+
+  useEffect(verifyReAuthorizationNeed, [currentUser?.walletAddress]);
+
   return (
-    <GlobalEffectsProvider>
+    <>
       <NetworkThemeInjector />
       {children}
-    </GlobalEffectsProvider>
+    </>
   );
 };
 
