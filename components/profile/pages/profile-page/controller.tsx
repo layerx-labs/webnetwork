9f394447a8d4f22b7725e6a731b8cd809e5cdcc1
MoshMage <moshmage@gmail.com>
2023-11-28 11:49:37 +0000

    replaces "githubLogin" for "handle" on the application

--- components/profile/pages/profile-page/controller.tsx
+++ components/profile/pages/profile-page/controller.tsx
@@ -1,6 +1,6 @@
 import ProfilePageView from "components/profile/pages/profile-page/view";
 
-import { useAppState } from "contexts/app-state";
+import {useAppState} from "contexts/app-state";
 
 export default function ProfilePage() {
   const { state } = useAppState();
