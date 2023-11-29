d39fc17ca505e90b6ce1ad1996d38ab0264aac6e
Marcus Vinícius <marcusvinici25@gmail.com>
2023-11-29 17:59:08 -0300

    removing userEvent

--- __tests__/components/profile/address-with-copy/address-with-copy.controller.test.tsx
+++ __tests__/components/profile/address-with-copy/address-with-copy.controller.test.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 import { screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 
 import CopyIcon from "assets/icons/copy-icon";
 
