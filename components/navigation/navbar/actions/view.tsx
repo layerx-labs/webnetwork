751d6d11b995847565f836ed3bf872f9e029e84c
Marcus Vinícius <marcusvinici25@gmail.com>
2023-12-07 18:29:55 -0300

    adjusting NavBarActions

--- components/navigation/navbar/actions/view.tsx
+++ components/navigation/navbar/actions/view.tsx
@@ -1,11 +1,11 @@
 import { useTranslation } from "next-i18next";
 
 import HelpButton from "components/common/buttons/help/view";
-import NotificationButton from "components/common/buttons/notification/view";
 import ConnectWalletButton from "components/connections/connect-wallet-button/connect-wallet-button.controller";
 import CreateNetworkBountyButton from "components/create-network-bounty-button/controller";
 import NavAvatar from "components/nav-avatar";
 import HamburgerButton from "components/navigation/hamburger/controller";
+import Notifications from "components/notifications/controller";
 import ResponsiveWrapper from "components/responsive-wrapper";
 import TransactionsStateIndicator from "components/transactions-state-indicator";
 
@@ -33,7 +33,7 @@ export default function NavBarActions() {
           >
             <div className="d-flex gap-3 align-items-center">
               <TransactionsStateIndicator />
-              <NotificationButton />
+              <Notifications />
               <NavAvatar />
             </div>
           </ResponsiveWrapper>
