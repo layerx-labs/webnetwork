c5fe0c339ba07595c8574149fc34fbf01d8102e0
Marcus Vinícius <marcusvinici25@gmail.com>
2023-12-06 21:34:21 -0300

    creating Notifications interface

---
+++ interfaces/notifications.ts
@@ -0,0 +1,12 @@
+export type NotificationsType = 'task' | 'deliverable' | 'proposal' | 'dispute' | 'open' | 'ready' | 'canceled'
+
+export interface Notifications {
+    id: number,
+    type: NotificationsType;
+    userId: number;
+    read: boolean;
+    uuid?: string;
+    hide: boolean;
+    createdAt: string | Date;
+    updatedAt: string | Date;
+}
