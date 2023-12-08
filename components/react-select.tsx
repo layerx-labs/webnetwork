7f5020ae1ad48b2361c0e90ff3b417daa750701a
vhcsilva <vhcsilvaa@gmail.com>
2023-12-08 10:35:54 -0300

    add wrapper with test-id on clear btn to make test easier

--- components/react-select.tsx
+++ components/react-select.tsx
@@ -1,12 +1,25 @@
 import Select from "react-select";
+import { components } from "react-select";
 
  //TODO: Need to rework components to be able to use Props correctly { Props }
+
+function ClearIndicatorWithTestId (props) {
+  return(
+    <div data-testid="react-select-clear-indicator">
+      <components.ClearIndicator {...props} />
+    </div>
+  );
+}
 export default function ReactSelect(params: any) { // eslint-disable-line 
   return (
     <Select
       className="react-select-container"
       classNamePrefix="react-select"
       {...params}
+      components={{
+        ...params?.components,
+        ClearIndicator: ClearIndicatorWithTestId
+      }}
     />
   );
 }
