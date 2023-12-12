24e7a1fb59b98b9b789fc615dcfb4a3443a48d90
Marcus Vinícius <marcusvinici25@gmail.com>
2023-12-12 20:21:17 -0300

    creating useUpdateUserSettings api/hook

---
+++ x-hooks/api/user/settings/use-update-settings.ts
@@ -0,0 +1,8 @@
+import { api } from "services/api";
+
+export async function useUpdateUserSettings(settings: {
+  notifications: boolean;
+  language: string;
+}) {
+  return api.post(`/user/settings`, { settings }).then(({ data }) => data);
+}
