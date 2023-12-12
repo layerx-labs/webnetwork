85f7e9d61791b427087b7a6968079714ae65f44d
Marcus Vinícius <marcusvinici25@gmail.com>
2023-12-12 20:21:01 -0300

    adjusting changeAddressSettings to create or change

--- server/common/user/settings/change-address-settings.ts
+++ server/common/user/settings/change-address-settings.ts
@@ -4,7 +4,7 @@ import models from "db/models";
 
 import {HttpBadRequestError, HttpUnauthorizedError} from "server/errors/http-errors";
 
-export function changeAddressSettings(req: NextApiRequest) {
+export async function changeAddressSettings(req: NextApiRequest) {
   const {context: {user}, settings} = req.body;
 
   if (!user?.id)
@@ -20,11 +20,22 @@ export function changeAddressSettings(req: NextApiRequest) {
   if (!Object.keys(settings).every(k => allowSettingsProps.includes(k)))
     throw new HttpBadRequestError();
 
-  if (settings.language && !/^([a-z]{2}|[A-Z]{2})$/.test(settings.language))
+  if (settings.language && !/^[a-zA-Z]{2}(-[a-zA-Z]{2})?$/.test(settings.language))
     throw new HttpBadRequestError();
 
   if (settings.notifications && ![true,false].includes(settings.notifications))
     throw new HttpBadRequestError()
+  
+  const [userSetting, created] = await models.userSetting.findOrCreate({
+    where: { userId: user.id },
+    defaults: settings
+  });
 
-  return models.userSetting.update(settings, {where: {userId: user.id}})
+  if (!created) {
+    if(settings.language) userSetting.language = settings.language
+    userSetting.notifications = settings.notifications
+    await userSetting.save()
+  }
+
+  return "updated Settings"
 }
