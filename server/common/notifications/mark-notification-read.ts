b65cf75748788e4559ba332728d5ba25992a889a
Marcus Vinícius <marcusvinici25@gmail.com>
2023-12-13 19:41:09 -0300

    adding IsUUID logic to mark-notification-read endpoint

--- server/common/notifications/mark-notification-read.ts
+++ server/common/notifications/mark-notification-read.ts
@@ -1,4 +1,7 @@
 import {NextApiRequest} from "next";
+import {validate as isUUID} from 'uuid'
+
+import models from "db/models";
 
 import {BadRequestErrors, NotFoundErrors} from "../../../interfaces/enums/Errors";
 import {HttpBadRequestError, HttpNotFoundError, HttpUnauthorizedError} from "../../errors/http-errors";
@@ -8,16 +11,32 @@ export async function markNotificationRead(req: NextApiRequest) {
   const {id, read} = req.query as {address?: string, read: "true"|"false", id?: string};
   const {context: {user:{id: userId}}} = req.body;
 
+  let currentId;
+
   if (!userId)
     throw new HttpUnauthorizedError();
 
-  if (!id || id && isNaN(+id))
-    throw new HttpBadRequestError(BadRequestErrors.WrongParamsNotANumber);
+  if (!id)
+    throw new HttpBadRequestError(BadRequestErrors.WrongParameters);
 
   if (["true", "false"].every(s => s !== read))
     throw new HttpBadRequestError(BadRequestErrors.WrongParameters);
 
-  const [notifications] = await getNotifications({query: {id}, body: req.body} as any);
+  if(isUUID(id)){
+    const notif = await models.notification.findOne({
+      where: {
+        uuid: id,
+        userId
+      }
+    })
+
+    currentId = +notif?.id
+  } else currentId = +id 
+
+  if(isNaN(currentId))
+    throw new HttpBadRequestError(BadRequestErrors.WrongParamsNotANumber);
+
+  const [notifications] = await getNotifications({query: {id: +currentId}, body: req.body} as any);
 
   if (!notifications)
     throw new HttpNotFoundError(NotFoundErrors.NotificationNotFound);
