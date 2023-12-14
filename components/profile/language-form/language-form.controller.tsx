6eb70bdf15d1139222965889a6602a980c591925
vhcsilva <vhcsilvaa@gmail.com>
2023-12-14 15:36:16 -0300

    make it refresh server side props

--- components/profile/language-form/language-form.controller.tsx
+++ components/profile/language-form/language-form.controller.tsx
@@ -1,25 +1,33 @@
 import { useSession } from "next-auth/react";
 import { useTranslation } from "next-i18next";
 import getConfig from "next/config";
+import { useRouter } from "next/router";
 
 import LanguageFormView from "components/profile/language-form/language-form.view";
 
 import { SelectOption } from "types/utils";
 
 import { useUpdateUserSettings } from "x-hooks/api/user/settings";
+import { useToastStore } from "x-hooks/stores/toasts/toasts.store";
 import { useUserStore } from "x-hooks/stores/user/user.store";
 import useReactQueryMutation from "x-hooks/use-react-query-mutation";
 
 const { publicRuntimeConfig } = getConfig();
 export default function LanguageForm () {
+  const router = useRouter();
   const session = useSession();
   const { t } = useTranslation(["common", "profile"]);
 
   const {currentUser} = useUserStore();
+  const { addSuccess } = useToastStore();
   const {mutate, isLoading} = useReactQueryMutation({
     mutationFn: (languageOption: SelectOption) =>
       useUpdateUserSettings({language: languageOption?.value?.toString()}),
-    onSuccess: () => session.update()
+    onSuccess: () => {
+      session.update();
+      router.replace(router.asPath);
+      addSuccess(t("actions.success"), t("profile:language-updated"));
+    }
   });
 
   const languages = publicRuntimeConfig?.locales?.map(locale => ({
