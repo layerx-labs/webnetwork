894ec04e0429ecbd5db8c676b2391e1e3bb0b718
Marcus Vinícius <marcusvinici25@gmail.com>
2023-12-15 11:43:06 -0300

    adjusting mocks

--- __mocks__/next/router/index.ts
+++ __mocks__/next/router/index.ts
@@ -1,9 +1,10 @@
-const useRouter = () => ({
+const useRouter = jest.fn(() => ({
   query: {},
   pathname: "",
   asPath: "",
+  events: { on: (event, callback) => callback(), off: jest.fn() },
   push: jest.fn()
-});
+}))
 
 export {
   useRouter
