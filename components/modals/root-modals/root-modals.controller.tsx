7a414af050aad36b4d15fe1c654627957082758f
vhcsilva <vhcsilvaa@gmail.com>
2024-01-08 10:42:28 -0300

    refactor modal structure

--- components/modals/root-modals/root-modals.controller.tsx
+++ components/modals/root-modals/root-modals.controller.tsx
@@ -2,7 +2,7 @@ import WalletSelectorModal from "components/connections/wallet-selector-modal/wa
 import InvalidAccountWalletModal from "components/invalid-account-wallet-modal";
 import NoWalletModal from "components/modals/no-wallet-modal/no-wallet-modal.controller";
 import WalletMismatchModal from "components/modals/wallet-mismatch/controller";
-import WrongNetworkModal from "components/wrong-network-modal";
+import WrongNetworkModal from "components/modals/wrong-network-modal/wrong-network-modal.controller";
 
 export default function RootModals() {
   return(
