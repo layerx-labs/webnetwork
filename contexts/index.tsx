ada7abdc90805ef20d4141edd6430588dc827a38
vhcsilva <vhcsilvaa@gmail.com>
2024-01-19 10:50:52 -0300

    should clear marketplace data when outside

--- contexts/index.tsx
+++ contexts/index.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 
 import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
 import NetworkThemeInjector from "components/custom-network/network-theme-injector";
 
@@ -8,17 +9,28 @@ import { useDaoStore } from "x-hooks/stores/dao/dao.store";
 import { useUserStore } from "x-hooks/stores/user/user.store";
 import { useAuthentication } from "x-hooks/use-authentication";
 import { useDao } from "x-hooks/use-dao";
+import useMarketplace from "x-hooks/use-marketplace";
 import { useSettings } from "x-hooks/use-settings";
 import useSupportedChain from "x-hooks/use-supported-chain";
 
 const RootProviders = ({ children }) => {
+  const router = useRouter();
   const session = useSession();
+
   const { loadSettings } = useSettings();
-  const { service: daoService } = useDaoStore();
-  const { currentUser } = useUserStore();
-  const { supportedChains } = useSupportedChain();
   const { listenChainChanged } = useDao();
+  const { clear, refresh } = useMarketplace();
+  const { supportedChains } = useSupportedChain();
   const { syncUserDataWithSession, updateWalletBalance, verifyReAuthorizationNeed } = useAuthentication();
+
+  const { currentUser } = useUserStore();
+  const { service: daoService } = useDaoStore();
+
+  useEffect(() => {
+    if (router?.pathname?.includes("[network")) return;
+    clear();
+    refresh();
+  }, [router?.pathname]);
 
   useEffect(() => {
     loadSettings()
