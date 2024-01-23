fdcea79e80f8b202cc19dadf0d542f5eb0b1e387
Marcus Vinícius <marcusvinici25@gmail.com>
2024-01-23 11:58:16 -0300

    adding new category icons

---
+++ assets/icons/pencil-line-icon.tsx
@@ -0,0 +1,58 @@
+import { SVGProps } from "react";
+
+export default function PencilLineIcon(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      width="110"
+      height="86"
+      viewBox="0 0 110 86"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <g
+        style={{ mixBlendMode: "soft-light" }}
+        clipPath="url(#clip0_4207_7382)"
+      >
+        <path
+          d="M92.8111 68.8124H41.2486L17.4053 44.9691"
+          stroke="white"
+          strokeWidth="5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M70.4688 15.5312L29.2188 56.7812"
+          stroke="white"
+          strokeWidth="5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M41.25 68.8124H20.625C19.7133 68.8124 18.839 68.4502 18.1943 67.8056C17.5497 67.1609 17.1875 66.2866 17.1875 65.3749V46.1722C17.1879 45.2617 17.5495 44.3886 18.193 43.7444L71.182 -9.24465C71.8266 -9.88882 72.7006 -10.2507 73.6119 -10.2507C74.5232 -10.2507 75.3972 -9.88882 76.0418 -9.24465L95.2445 9.9452C95.8887 10.5898 96.2506 11.4638 96.2506 12.3751C96.2506 13.2864 95.8887 14.1604 95.2445 14.805L41.25 68.8124Z"
+          stroke="white"
+          strokeWidth="5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M58.4375 3.5L82.5 27.5625"
+          stroke="white"
+          strokeWidth="5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_4207_7382">
+          <rect
+            width="110"
+            height="110"
+            fill="white"
+            transform="translate(0 -24)"
+          />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
