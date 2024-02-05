064fde4c7366df9aa1f95fae6d2bbd33726488d8
vhcsilva <vhcsilvaa@gmail.com>
2024-02-05 09:27:38 -0300

    add link to public profile

--- components/profile/pages/profile-page/view.tsx
+++ components/profile/pages/profile-page/view.tsx
@@ -6,6 +6,7 @@ import AvatarOrIdenticon from "components/avatar-or-identicon";
 import Badge from "components/badge";
 import CustomContainer from "components/custom-container";
 import { Divider } from "components/divider";
+import InternalLink from "components/internal-link";
 import AddressWithCopy from "components/profile/address-with-copy/controller";
 import DashboardLayout from "components/profile/dashboard-layout";
 import LanguageForm from "components/profile/language-form/language-form.controller";
@@ -28,7 +29,7 @@ export default function ProfilePageView({
 
   const { isMobileView, isTabletView } = useBreakPoint();
 
-  const isTabletOrMobile = isMobileView || isTabletView ? true : false;
+  const isTabletOrMobile = isMobileView || isTabletView;
 
   return (
     <>
@@ -54,13 +55,13 @@ export default function ProfilePageView({
                 withBorder
               />
             </div>
-              <UserNameForm />
-              <div className={`${isTabletOrMobile ? "ms-2" : "mt-2" } text-truncate`}>
-                    <AddressWithCopy
-                      address={walletAddress}
-                      textClass="caption-medium font-weight-normal text-capitalize text-gray-300"
-                      truncated
-                    />
+            <UserNameForm />
+            <div className={`${isTabletOrMobile ? "ms-2" : "mt-2" } text-truncate`}>
+              <AddressWithCopy
+                address={walletAddress}
+                textClass="caption-medium font-weight-normal text-capitalize text-gray-300"
+                truncated
+              />
               {isCouncil && (
                 <Badge
                   label={t("profile:council")}
@@ -68,8 +69,14 @@ export default function ProfilePageView({
                   className="caption border border-purple text-purple border-radius-8 mt-3"
                 />
               )}
-              </div>
             </div>
+
+            <InternalLink
+              label={"Go to public profile"}
+              href={`/profile/${walletAddress}`}
+              className="mt-3"
+            />
+          </div>
         </div>
         <NotificationForm />
         <LanguageForm />
