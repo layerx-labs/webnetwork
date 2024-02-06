68d9619a92497a5827678fe86af34b05ca3b93db
vhcsilva <vhcsilvaa@gmail.com>
2024-02-06 11:13:05 -0300

    add view component

--- components/pages/public-profile/public-profile.controller.tsx
+++ components/pages/public-profile/public-profile.controller.tsx
@@ -3,13 +3,7 @@ import React from "react";
 import { useTranslation } from "next-i18next";
 import { useRouter } from "next/router";
 
-import AvatarOrIdenticon from "components/avatar-or-identicon";
-import CustomContainer from "components/custom-container";
-import If from "components/If";
-import DeliverablesList from "components/lists/deliverables/deliverables-list.controller";
-import ProposalsList from "components/lists/proposals/proposals-list.controller";
-import TasksList from "components/lists/tasks/controller";
-import ScrollableTabs from "components/navigation/scrollable-tabs/view";
+import PublicProfileView from "components/pages/public-profile/public-profile.view";
 
 import { truncateAddress } from "helpers/truncate-address";
 
@@ -34,22 +28,14 @@ export default function PublicProfilePage ({
 }: PublicProfilePageProps) {
   const { t } = useTranslation("profile");
   const { query, pathname, asPath, push } = useRouter();
-  const { isMobileView, isTabletView } = useBreakPoint();
 
   const type = query?.type?.toString() || "won";
   const isTaskList = ["won", "opened"].includes(type);
   const isDeliverableList = type === "submissions";
   const isProposalsList = type === "proposals";
-  const isTabletOrMobile = isMobileView || isTabletView;
   const hasHandle = !!user?.handle;
   const truncatedAddress = truncateAddress(user?.address || "");
   const [primaryText, secondaryText] = hasHandle ? [user?.handle, truncatedAddress] : [truncatedAddress, user?.handle];
-  const listItemVariant = ({
-    won: "network",
-    opened: "network",
-    submissions: "submissions",
-    proposals: "proposals"
-  }[type] || "network") as TasksListItemVariant;
 
   const getTab = (label: string, key: string) => ({
     label,
@@ -68,70 +54,20 @@ export default function PublicProfilePage ({
     getTab(t("proposals"), "proposals"),
     getTab(t("tasks-opened"), "opened"),
   ];
-  
+
   return (
-    <CustomContainer
-      col="col-xs-12 col-xl-12"
-      className="mt-4 pt-2 mt-lg-5 pt-lg-2"
-    >
-      <div className="row align-items-center mb-5">
-        <div className="col-auto">
-          <AvatarOrIdenticon
-            address={user?.address}
-            size={isTabletOrMobile ? 56 : "lg"}
-            withBorder
-          />
-        </div>
-
-        <div className="col">
-          <div className="row">
-            <h1 className="xl-semibold font-weight-medium text-white">
-              {primaryText}
-            </h1>
-          </div>
-
-          <If condition={!!secondaryText}>
-            <div className="row">
-              <h2 className="sm-regular font-weight-normal text-gray-300">
-                {secondaryText}
-              </h2>
-            </div>
-          </If>
-        </div>
-      </div>
-
-      <div className="row mb-4 pb-1">
-        <div className="col">
-          <ScrollableTabs
-            tabs={tabs}
-          />
-        </div>
-      </div>
-
-      <div className="row">
-        <div className="col">
-          <If condition={isTaskList}>
-            <TasksList
-              bounties={tasks}
-              variant="profile"
-              itemVariant={listItemVariant}
-              hideTitle
-            />
-          </If>
-
-          <If condition={isDeliverableList}>
-            <DeliverablesList
-              deliverables={deliverables}
-            />
-          </If>
-
-          <If condition={isProposalsList}>
-            <ProposalsList
-              proposals={proposals}
-            />
-          </If>
-        </div>
-      </div>
-    </CustomContainer>
+    <PublicProfileView
+      userAddress={user?.address}
+      primaryText={primaryText}
+      secondaryText={secondaryText}
+      tabs={tabs}
+      isTaskList={isTaskList}
+      isDeliverableList={isDeliverableList}
+      isProposalsList={isProposalsList}
+      tasks={tasks}
+      deliverables={deliverables}
+      proposals={proposals}
+      type={type}
+    />
   );
 }
