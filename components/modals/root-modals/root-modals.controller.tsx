1bef589e59126ee8ff17d3e9c28ebf0b304a8e09
vhcsilva <vhcsilvaa@gmail.com>
2024-02-21 11:03:43 -0300

    remove in favor of rainbowkit and wagmi

--- components/modals/root-modals/root-modals.controller.tsx
+++ components/modals/root-modals/root-modals.controller.tsx
@@ -1,17 +1,13 @@
-import WalletSelectorModal from "components/connections/wallet-selector-modal/wallet-selector-modal.controller";
 import InvalidAccountWalletModal from "components/invalid-account-wallet-modal";
-import NoWalletModal from "components/modals/no-wallet-modal/no-wallet-modal.controller";
 import WalletMismatchModal from "components/modals/wallet-mismatch/wallet-mismatch-modal.controller";
 import WrongNetworkModal from "components/modals/wrong-network-modal/wrong-network-modal.controller";
 
 export default function RootModals() {
   return(
     <>
-      <NoWalletModal />
       <InvalidAccountWalletModal />
       <WrongNetworkModal />
       <WalletMismatchModal />
-      <WalletSelectorModal />
     </>
   );
 }
