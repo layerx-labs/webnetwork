09babd79299de08edba9944f4f5891ba01711aa5
vhcsilva <vhcsilvaa@gmail.com>
2024-03-06 17:42:20 -0300

    add loader to only ask for sign if is waiting for it

--- contexts/authentication.tsx
+++ contexts/authentication.tsx
@@ -3,7 +3,9 @@ import { ReactNode, useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
 import { useAccount } from "wagmi";
 
+import { useLoadersStore } from "x-hooks/stores/loaders/loaders.store";
 import { useAuthentication } from "x-hooks/use-authentication";
+import { useDao } from "x-hooks/use-dao";
 
 interface AuthenticationProviderProps {
   children?: ReactNode;
@@ -14,17 +16,24 @@ export function AuthenticationProvider ({ children }: AuthenticationProviderProp
 
   const [isSigning, setIsSigning] = useState(false);
 
+  const { disconnect } = useDao();
   const { signInWallet } = useAuthentication();
+  const { waitingForMessageSign } = useLoadersStore();
+
+  const isWalletConnected =  account?.status === "connected";
+  const isAuthenticated = session?.status === "authenticated";
 
   useEffect(() => {
-    const isWalletConnected =  account?.status === "connected";
-    const isUnauthenticated = session?.status === "unauthenticated";
-    if (!isWalletConnected || !isUnauthenticated || isSigning)
+    if (isSigning || !isWalletConnected || isAuthenticated) 
       return;
-    setIsSigning(true);
-    signInWallet()
-      .finally(() => setIsSigning(false));
-  }, [session?.status, account?.status]);
+    if (!waitingForMessageSign) 
+      disconnect(); 
+    else {
+      setIsSigning(true);
+      signInWallet()
+        .finally(() => setIsSigning(false));
+    }
+  }, [isWalletConnected, isAuthenticated]);
   
   return(
     <>
--- x-hooks/stores/loaders/loaders.store.ts
+++ x-hooks/stores/loaders/loaders.store.ts
@@ -4,21 +4,16 @@ import { LoadingState } from "interfaces/loading-state";
 
 type LoadersStore = {
   loading: LoadingState | null;
-  web3Dialog: boolean;
-  missingMetamask: boolean;
   wrongNetworkModal: boolean;
   walletMismatchModal: boolean;
-  walletSelectorModal: boolean;
+  waitingForMessageSign: boolean;
   updateLoading: (loading: LoadingState) => void;
-  updateWeb3Dialog: (web3Dialog: boolean) => void;
-  updateMissingMetamask: (missingMetamask: boolean) => void;
   updateWrongNetworkModal: (wrongNetworkModal: boolean) => void;
   updateWalletMismatchModal: (walletMismatchModal: boolean) => void;
-  updateWalletSelectorModal: (walletMismatchModal: boolean) => void;
+  updateWaitingForMessageSign: (waitingForMessageSign: boolean) => void;
 };
 
-type Properties = "loading" | "web3Dialog" | "missingMetamask" |
-  "wrongNetworkModal" | "walletMismatchModal" | "walletSelectorModal";
+type Properties = "loading" | "wrongNetworkModal" | "walletMismatchModal" | "waitingForMessageSign";
 
 const updateState = (property: Properties, value: boolean | LoadingState) =>
   (state: LoadersStore) => ({
@@ -28,17 +23,14 @@ const updateState = (property: Properties, value: boolean | LoadingState) =>
 
 export const useLoadersStore = create<LoadersStore>((set) => ({
   loading: null,
-  web3Dialog: false,
-  missingMetamask: false,
   wrongNetworkModal: false,
   walletMismatchModal: false,
   walletSelectorModal: false,
+  waitingForMessageSign: false,
   updateLoading: (loading: LoadingState) => set(updateState("loading", loading)),
-  updateWeb3Dialog: (web3Dialog: boolean) => set(updateState("web3Dialog", web3Dialog)),
-  updateMissingMetamask: (missingMetamask: boolean) => set(updateState("missingMetamask", missingMetamask)),
   updateWrongNetworkModal: (wrongNetworkModal: boolean) => set(updateState("wrongNetworkModal", wrongNetworkModal)),
   updateWalletMismatchModal: (walletMismatchModal: boolean) =>
     set(updateState("walletMismatchModal", walletMismatchModal)),
-  updateWalletSelectorModal: (walletSelectorModal: boolean) =>
-    set(updateState("walletSelectorModal", walletSelectorModal)),
+  updateWaitingForMessageSign: (waitingForMessageSign: boolean) =>
+    set(updateState("waitingForMessageSign", waitingForMessageSign)),
 }));
