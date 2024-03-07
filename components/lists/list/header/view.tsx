cdaaa6b655774e5cd8bce63d62efc36c32a863cf
vhcsilva <vhcsilvaa@gmail.com>
2024-03-07 17:48:53 -0300

    add tips to curators list

--- components/lists/list/header/view.tsx
+++ components/lists/list/header/view.tsx
@@ -1,19 +1,26 @@
+import { Tooltip } from "components/common/tooltip/tooltip.view";
+
+import { HeaderColumn } from "types/components";
+
 interface ListHeaderProps {
-  columns: string[];
+  columns: HeaderColumn[];
 }
 
 export default function ListHeader({
   columns
 }: ListHeaderProps) {
+  function renderListBarColumn(column: HeaderColumn, key: number) {
+    const LabelWrapper = ({ children }) => 
+      column?.tip ? <Tooltip tip={column?.tip}>{children}</Tooltip> : <>{children}</>;
 
-  function renderListBarColumn(label: string, key: number) {
     return (
       <div
-        key={`${key}-${label}`}
-        className={`d-flex flex-row col justify-content-center  align-items-center 
-        text-gray`}
+      key={`${key}-${column?.label}`}
+      className={`d-flex flex-row col justify-content-center align-items-center text-gray`}
       >
-        <span className="xs-medium text-uppercase text-center gray-150">{label}</span>
+        <LabelWrapper>
+          <span className="xs-medium text-uppercase text-center gray-150">{column?.label}</span>
+        </LabelWrapper>
       </div>
     );
   }
