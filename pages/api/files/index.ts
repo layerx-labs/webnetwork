815e81256aa0942bc8ce466f8eda6a1889935bc8
MoshMage <moshmage@gmail.com>
2024-03-14 10:40:18 +0000

    change file upload action

--- pages/api/files/index.ts
+++ pages/api/files/index.ts
@@ -4,26 +4,20 @@ import {withProtected} from "middleware";
 
 import {error as LogError, Logger} from "services/logging";
 
-import {post} from "server/common/file/post";
-
-export const config = {
-  api: {
-    bodyParser: false
-  }
-};
+import {uploadFiles} from "server/common/file/post";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
     switch (req.method.toLowerCase()) {
     case "post":
-      res.status(200).json(await post(req));
+      res.status(200).json(await uploadFiles(req));
       break;
 
     default:
       res.status(405);
     }
   } catch (error) {
-    LogError(error);
+    LogError(`Error uploading files`, error);
     res.status(error?.status || 500).json(error?.message || error?.toString());
   }
 
--- server/common/file/post.ts
+++ server/common/file/post.ts
@@ -1,10 +1,32 @@
 import formidable from "formidable";
 import fs from "fs";
-import { NextApiRequest } from "next";
+import {NextApiRequest} from "next";
 
 import IpfsStorage from "services/ipfs-service";
 
-import { HttpBadRequestError } from "server/errors/http-errors";
+import {HttpBadRequestError} from "server/errors/http-errors";
+
+import {debug} from "../../../services/logging";
+
+export async function uploadFiles(req: NextApiRequest) {
+
+  console.log(`\n\n BODY`,req.body,`\n\n`)
+
+  if (!req.body || !req.body.files)
+    throw new HttpBadRequestError("Missing files");
+
+  const {files} = req.body;
+
+  try {
+    return Promise.all(files.map(file => {
+      const fileName = `${new Date().toISOString()}-${file.fileName}`;
+      return IpfsStorage.add(Buffer.from(file.fileData, "base64"), true, fileName);
+    }))
+  } catch (e) {
+    debug(`Error uploading file to IPFS`, e.message);
+    throw new HttpBadRequestError("Something went wrong.");
+  }
+}
 
 export async function post(req: NextApiRequest) {
   const formData = await new Promise<{ fields; files: object }>((resolve, reject) => {
