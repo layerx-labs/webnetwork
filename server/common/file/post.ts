70e95528910d9947affd7c340dec0f611a0aadc2
MoshMage <moshmage@gmail.com>
2024-03-14 12:30:40 +0000

    make request-size return instead of throw; make file/post.ts upload just the base64data and check for mime type

--- server/common/file/post.ts
+++ server/common/file/post.ts
@@ -1,5 +1,3 @@
-import formidable from "formidable";
-import fs from "fs";
 import {NextApiRequest} from "next";
 
 import IpfsStorage from "services/ipfs-service";
@@ -10,43 +8,32 @@ import {debug} from "../../../services/logging";
 
 export async function uploadFiles(req: NextApiRequest) {
 
-  console.log(`\n\n BODY`,req.body,`\n\n`)
-
   if (!req.body || !req.body.files)
     throw new HttpBadRequestError("Missing files");
 
   const {files} = req.body;
 
+  // prepare before starting the Promise.all, so we can escape earlier
+  const preparedPayloads = files.map(({fileName, fileData}) => {
+    const name = `${new Date().toISOString()}-${fileName}`;
+    const [, ext] = name.split(/\.(?=[^.]*$)/g);
+    const [mime, data] = fileData.split(",");
+
+    const acceptedMimes = [/image\/(jpeg|png|gif|svg\+xml)/, /application\/pdf/];
+
+    const validMime = acceptedMimes.some(pattern => pattern.test(mime));
+
+    if (!validMime)
+      throw new HttpBadRequestError("invalid mime");
+
+    return {name, ext, data}
+  });
+
   try {
-    return Promise.all(files.map(file => {
-      const fileName = `${new Date().toISOString()}-${file.fileName}`;
-      return IpfsStorage.add(Buffer.from(file.fileData, "base64"), true, fileName);
-    }))
+    return Promise.all(preparedPayloads.map(({name, ext, data}) =>
+      IpfsStorage.add(Buffer.from(data, "base64url"), true, name, ext)));
   } catch (e) {
     debug(`Error uploading file to IPFS`, e.message);
     throw new HttpBadRequestError("Something went wrong.");
   }
-}
-
-export async function post(req: NextApiRequest) {
-  const formData = await new Promise<{ fields; files: object }>((resolve, reject) => {
-    new formidable.IncomingForm().parse(req, (err, fields, files) => {
-      if (err) {
-        return reject(err);
-      }
-      return resolve({ fields, files });
-    });
-  });
-
-  const values = Object.values(formData.files);
-
-  if (values.length < 1)
-    throw new HttpBadRequestError("Missing files");
-
-  const uploadFiles = values.map(async (file) => 
-    IpfsStorage.add(fs.readFileSync(file.filepath), false, file.originalFilename));
-
-  const files = await Promise.all(uploadFiles);
-
-  return files;
 }
