ac5c0121c327d90e91fe7733f25869622125594c
MoshMage <moshmage@gmail.com>
2024-03-19 10:55:07 +0000

    forgot to change the pointer for seoImage as well

--- pages/[network]/task/[id]/index.tsx
+++ pages/[network]/task/[id]/index.tsx
@@ -146,7 +146,7 @@ export const getServerSideProps: GetServerSideProps = async ({ req, query, local
     title: bountyData?.title,
     body: bountyData?.body,
     id: bountyData?.id,
-    nftImage: bountyData.seoImage,
+    seoImage: bountyData.seoImage,
   };
 
   return {
