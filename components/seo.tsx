46a82b1471b8bc2ba3fc4fdd8de7b2d3a99d1fab
MoshMage <moshmage@gmail.com>
2024-03-19 11:07:00 +0000

    use configured value from environment instead

--- components/seo.tsx
+++ components/seo.tsx
@@ -24,7 +24,7 @@ const Seo: React.FC<ISeoProps> = ({ issueMeta }) => {
     const issueId = issueMeta?.id;
     const hash = issueMeta?.seoImage;
 
-    const imageUrl = hash ? `https://ipfs.io/ipfs/${hash}` : `${homeUrl}/images/meta-thumbnail.jpeg`;
+    const imageUrl = hash ? `${publicRuntimeConfig.urls.ipfs}/${hash}` : `${homeUrl}/images/meta-thumbnail.jpeg`;
 
     const description = removeMarkdown(issueMeta?.body?.substring(0, 160).trimEnd());
 
