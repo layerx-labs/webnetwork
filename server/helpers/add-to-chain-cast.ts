ab34793fcec5282b2de533a2d25f0f693982a178
vhcsilva <vhcsilvaa@gmail.com>
2024-04-01 12:25:59 -0300

    generate base64 program

--- server/helpers/add-to-chain-cast.ts
+++ server/helpers/add-to-chain-cast.ts
@@ -1,6 +1,22 @@
 import NetworkRegistry from "@taikai/dappkit/dist/build/contracts/NetworkRegistry.json";
+import getConfig from "next/config";
 
 import { ChainCastService } from "server/services/chain-cast";
+
+const { serverRuntimeConfig } = getConfig();
+
+function getBase64Program(type: "registry" | "network") {
+  return  Buffer.from(JSON.stringify([{
+    "name": "bullmq-producer",
+    "args": {
+      "bodyInput": "event",
+      "queueName": `${serverRuntimeConfig?.environmentName}_${type}_queue`,
+      "indexName": "txin",
+      "redisHost": serverRuntimeConfig?.chainCast?.redisHost,
+      "redisPort": serverRuntimeConfig?.chainCast?.redisPort
+    }
+  }])).toString("base64");
+}
 
 export async function addToChainCast( type: "registry" | "network", 
                                       address: string, 
@@ -11,12 +27,13 @@ export async function addToChainCast( type: "registry" | "network",
   }[type];
 
   const base64Abi = Buffer.from(abi).toString("base64");
+  const base64Program = getBase64Program(type);
 
   return ChainCastService.addContractCast({
     abi: base64Abi,
     address,
     chainId: +chainId,
-    program: "W3sibmFtZSI6IndlYmhvb2siLCJhcmdzIjp7InVybCI6Imh0dHBzOi8vd2ViaG9vay5zaXRlLzEwOGZiMDFkLTVjM2MtNDg0Mi1iMjk2LTY2YzQzYmEyMmFmMCIsImJvZHlJbnB1dCI6ImV2ZW50In19XQ",
+    program: base64Program,
     type: "CUSTOM",
     startFrom: +startBlock,
   });
