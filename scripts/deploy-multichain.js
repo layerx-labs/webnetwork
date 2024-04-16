f76583edbf196b570ec272d9db9fbee79fa07290
vhcsilva <vhcsilvaa@gmail.com>
2024-04-16 12:50:05 -0300

    test other rpc

--- scripts/deploy-multichain.js
+++ scripts/deploy-multichain.js
@@ -25,7 +25,7 @@ const _xNetworks = {
   ... _xNetwork(`afrodite`, [`https://eth-afrodite.taikai.network:8080`], `TETH`, 1501, `Afrodite Test Chain`, `afrodite`, `https://eth-afrodite.taikai.network:8080`, `https://afrodite.taikai.network:2053`),
   ... _xNetwork(`irene`, [`https://eth-irene.taikai.network:8080`], `TETH`, 1502, `Irene Test Chain`, `irene`, `https://eth-irene.taikai.network:8080`, `https://irene.taikai.network:2053`),
   ... _xNetwork(`apollodorus`, [`https://eth-apollodorus.taikai.network:8080`], `TETH`, 1506, `Apollodorus Test Chain`, `apollodorus`, `https://eth-apollodorus.taikai.network:8080`, `https://apollodorus.taikai.network:2053`),
-  ... _xNetwork(`amoy`, [`https://rpc-amoy.polygon.technology/`], `MATIC`, 80002, `Amoy`, `amoy`, `https://www.oklink.com/amoy/tx`, `https://apollodorus.taikai.network:2053`),
+  ... _xNetwork(`amoy`, [`https://polygon-amoy-bor-rpc.publicnode.com`], `MATIC`, 80002, `Amoy`, `amoy`, `https://www.oklink.com/amoy/tx`, `https://apollodorus.taikai.network:2053`),
 }
 
 const options = yargs(hideBin(process.argv))
