4bdec9ba4034ba21d5a0b7f94b00216257ce46ab
MoshMage <moshmage@gmail.com>
2024-04-24 08:53:41 +0100

    fix migration

--- db/migrations/20240423104700-add-socials-column-to-user-table.js
+++ db/migrations/20240423104700-add-socials-column-to-user-table.js
@@ -3,8 +3,8 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.addColumn("users", "github_link", {type: Sequelize.STRING,});
-    await queryInterface.addColumn("users", "linkedin_link", {type: Sequelize.STRING,});
+    await queryInterface.addColumn("users", "githubLink", {type: Sequelize.STRING,});
+    await queryInterface.addColumn("users", "linkedInLink", {type: Sequelize.STRING,});
   },
 
   async down (queryInterface, Sequelize) {
