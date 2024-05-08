5b6b1b2e815f670341b9bfaa59e85cc9f241eabf
vhcsilva <vhcsilvaa@gmail.com>
2024-05-08 15:06:05 -0300

    handle update changed columns

--- components/administration/points-system-administration/points-system-administration.controller.tsx
+++ components/administration/points-system-administration/points-system-administration.controller.tsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 
+import { AxiosError } from "axios";
+
 import Button from "components/button";
+import { Divider } from "components/divider";
 import InputNumber from "components/input-number";
 import { Table } from "components/table/table.controller";
 
@@ -9,6 +12,7 @@ import { QueryKeys } from "helpers/query-keys";
 import { PointsBase } from "interfaces/points-system";
 
 import { useBulkUpdatePointsBase, useGetPointsBase } from "x-hooks/api/points";
+import { useToastStore } from "x-hooks/stores/toasts/toasts.store";
 import useReactQuery from "x-hooks/use-react-query";
 import useReactQueryMutation from "x-hooks/use-react-query-mutation";
 
@@ -20,20 +24,49 @@ type Header = {
 export function PointsSystemAdministration() {
   const [list, setList] = useState<PointsBase[]>([]);
   const [scalingFactor, setScalingFactor] = useState();
+  const [changedRows, setChangedRows] = useState<PointsBase[]>([]);
+
+  const { addError } = useToastStore();
   
-  const { data: pointsBase } = useReactQuery(QueryKeys.pointsBase(), async () => {
+  useReactQuery(QueryKeys.pointsBase(), async () => {
     const data = await useGetPointsBase();
     setList(data);
     return data;
   });
+
+  function getUpdateMethod(type: "bulk" | "changed") {
+    const rows = {
+      bulk: list.map(({ id, actionName }) => ({ id, actionName, scalingFactor })),
+      changed: changedRows
+    }[type];
+
+    return async () => useBulkUpdatePointsBase({
+      rows,
+    });
+  }
+
   const { mutate: hanleBulkUpdate, isPending: isBulkUpdating } = useReactQueryMutation({
     queryKey: QueryKeys.pointsBase(),
-    mutationFn: async () => {
-      return useBulkUpdatePointsBase({
-        rows: list.map(({ id }) => ({ id, scalingFactor })),
-      });
+    mutationFn: getUpdateMethod("bulk"),
+    onSettled: (data, error) => {
+      if (error) {
+        addError("Failed", "Scaling factor must be greater than 0");
+        return;
+      }
+      setScalingFactor(null);
     },
-    onSuccess: () => setScalingFactor(null)
+  });
+
+  const { mutate: handleChangedUpdate, isPending: isUpdatingChanged } = useReactQueryMutation({
+    queryKey: QueryKeys.pointsBase(),
+    mutationFn: getUpdateMethod("changed"),
+    onSettled: (data, error: AxiosError<{ message: string }>) => {
+      if (error) {
+        addError("Failed", `${error?.response?.data?.message}`);
+        return;
+      }
+      setChangedRows([]);
+    }
   });
 
   const headers: Header[] = [
@@ -44,7 +77,26 @@ export function PointsSystemAdministration() {
   ];
 
   const onScalingFactorChange = values => setScalingFactor(values.value);
-  const isUpdateButtonDisabled = !scalingFactor || isBulkUpdating || !list?.length;
+  const isUpdateButtonDisabled = !scalingFactor || isBulkUpdating || !list?.length || isUpdatingChanged;
+  const isSaveChangesButtonDisabled = !changedRows?.length || isUpdatingChanged || isBulkUpdating;
+
+  function onRowChange(row: PointsBase) {
+    setChangedRows(previous => {
+      const tmp = [...previous];
+      const index = tmp.findIndex(i => i.id === row.id);
+      if (index > -1)
+        tmp[index] = row;
+      else
+        tmp.push(row);
+      return tmp;
+    });
+    setList(previous => {
+      const tmp = [...previous];
+      const index = tmp.findIndex(i => i.id === row.id);
+      tmp[index] = row;
+      return tmp;
+    });
+  }
 
   return(
     <div className="pt-3">
@@ -69,6 +121,24 @@ export function PointsSystemAdministration() {
           </Button>
         </div>
       </div>
+
+      <Divider />
+
+      <div className="row align-items-center justify-content-end">
+        <div className="col col-md-auto">
+          <div className="row mx-0">
+            <Button
+              disabled={isSaveChangesButtonDisabled}
+              isLoading={isUpdatingChanged}
+              onClick={handleChangedUpdate}
+              className="mb-3"
+            >
+              Save changes
+            </Button>
+          </div>
+        </div>
+      </div>
+
       <Table
         variant="dark"
         borderless
@@ -77,6 +147,7 @@ export function PointsSystemAdministration() {
         headers={headers}
         rows={list}
         editableColumns={["scalingFactor", "pointsPerAction", "counter"]}
+        onRowChange={onRowChange}
       />
     </div>
   );
