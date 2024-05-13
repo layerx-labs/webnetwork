87486ca0a8acc6bc9ea2e72a3ef76fe09a641f80
MoshMage <moshmage@gmail.com>
2024-05-13 10:11:57 +0100

    use react query instead o mutation

--- components/profile/about-form/about-form-controller.tsx
+++ components/profile/about-form/about-form-controller.tsx
@@ -1,51 +1,46 @@
-import {useEffect, useState} from "react";
+import {useState} from "react";
 
 import {useSession} from "next-auth/react";
 import {useTranslation} from "next-i18next";
 
+import {QueryKeys} from "../../../helpers/query-keys";
 import {api} from "../../../services/api";
+import {useUserStore} from "../../../x-hooks/stores/user/user.store";
+import useReactQuery from "../../../x-hooks/use-react-query";
 import useReactQueryMutation from "../../../x-hooks/use-react-query-mutation";
 import {AboutFormView} from "./about-form-view";
 
 export function AboutForm() {
   const { t } = useTranslation(["profile"]);
   const { data: sessionData,} = useSession();
+  const {currentUser} = useUserStore()
 
   const [about, setAbout] = useState<string>("");
-  const [defaultValue, setDefaultValue] = useState<string>("");
 
   const onSaveSetAbout = () =>
-    api.put(`/user/${(sessionData?.user as any)?.address}/about`, {about: about})
+    api.put(`/user/${currentUser?.walletAddress}/about`, {about: about})
 
-  const _getUserAbout = () =>
-    api.get(`/user/${(sessionData?.user as any)?.address}/about`)
+  const getUserAbout = () =>
+    api.get(`/user/${currentUser?.walletAddress}/about`)
       .then(({data}) => {
-        setDefaultValue(data);
-        setAbout(data);
+        return data;
       });
 
-  const {mutateAsync: getUserAbout} = useReactQueryMutation({
-    mutationFn: _getUserAbout,
-  })
+  const {data: defaultAbout, invalidate: invalidateAbout} =
+    useReactQuery(QueryKeys.about(), getUserAbout)
 
   const {mutateAsync: saveAbout, isPending} = useReactQueryMutation({
     mutationFn: onSaveSetAbout,
     toastError: t("social.about.saving.error"),
     toastSuccess: t("social.about.saving.success"),
     onSuccess: () => {
-      getUserAbout(null)
+      invalidateAbout()
     },
   })
 
-  useEffect(() => {
-    if (!sessionData)
-      return;
-    getUserAbout(null);
-  }, [sessionData]);
-
   return <AboutFormView isBodyOverLimit={about.length > 512}
                         body={about}
-                        defaultValue={defaultValue}
+                        defaultValue={defaultAbout}
                         onChange={(e) => setAbout(e.target.value)}
                         isSaving={isPending}
                         onSave={saveAbout} />
