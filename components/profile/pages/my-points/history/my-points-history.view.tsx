9068d15f51664b3bb9bedce62269b6af82cc1c08
MoshMage <moshmage@gmail.com>
2024-05-16 13:28:01 +0100

    add pointsbadge to history

--- components/profile/pages/my-points/history/my-points-history.view.tsx
+++ components/profile/pages/my-points/history/my-points-history.view.tsx
@@ -2,6 +2,7 @@ import {format} from "date-fns";
 import {useTranslation} from "next-i18next";
 
 import {PointsHistory} from "../../../../../types/pages";
+import {PointsBadge} from "../../../../common/points/points-badge.view";
 
 export function MyPointsHistoryView({history}: {history: PointsHistory}) {
   const { t } = useTranslation(["profile"]);
@@ -19,7 +20,7 @@ export function MyPointsHistoryView({history}: {history: PointsHistory}) {
       history.map((entry, i) =>
         <div className="row mb-2 border-radius-8 p-3 bg-gray-900 border-gray-800" key={i}>
           <div className="col col-4 font-weight-600"><strong>{ph(`actionName.${entry.actionName}`)}</strong></div>
-          <div className="col col-3">{entry.pointsWon}</div>
+          <div className="col col-3 d-flex"><PointsBadge points={entry.pointsWon}/></div>
           <div className="col col-2">{ph(`state.${entry.pointsCounted?"counted":"pending"}`)}</div>
           <div className="col col-2 d-flex justify-content-end">{format(new Date(entry.updatedAt), "dd/MM/yyyy")}</div>
           <div className="col col-1"></div>
