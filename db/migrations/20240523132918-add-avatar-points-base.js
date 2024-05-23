e2eaf86efeac54a406155046f6c85f3f309beca4
vhcsilva <vhcsilvaa@gmail.com>
2024-05-23 10:30:58 -0300

    create migration to add new rule

---
+++ db/migrations/20240523132918-add-avatar-points-base.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert("points_base", [
+      {
+        actionName: "add_avatar",
+        pointsPerAction: 10,
+        scalingFactor: 1,
+        counter: "1",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("points_base", {actionName: ["add_avatar"]});
+  }
+};
