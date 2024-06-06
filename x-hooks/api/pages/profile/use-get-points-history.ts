4f281eac97313c774c82caa2e1da2a796a9bf09e
vhcsilva <vhcsilvaa@gmail.com>
2024-06-06 10:24:00 -0300

    add type

--- x-hooks/api/pages/profile/use-get-points-history.ts
+++ x-hooks/api/pages/profile/use-get-points-history.ts
@@ -1,6 +1,8 @@
+import { PointsEvents } from "interfaces/points";
+
 import {api} from "../../../../services/api";
 
-export async function useGetPointsHistory(userAddress: string) {
+export async function useGetPointsHistory(userAddress: string): Promise<PointsEvents[]> {
   return api.get(`/user/${userAddress}/points/history`)
     .then(({data}) => data)
     .catch(e => {
