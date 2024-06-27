b808107232c8f4e4c8036f28492aac1b320a685f
vhcsilva <vhcsilvaa@gmail.com>
2024-06-27 18:00:17 -0300

    refactor to use upload to ipfs hook

--- components/markdown-editor/upload-command/upload-command.controller.tsx
+++ components/markdown-editor/upload-command/upload-command.controller.tsx
@@ -1,22 +1,15 @@
 import { ChangeEvent, useRef } from "react";
 
-import getConfig from "next/config";
-
 import FileUpload from "assets/icons/file-upload"
 
 import If from "components/If";
 
-import { useUploadFile } from "x-hooks/api/file";
-import { useToastStore } from "x-hooks/stores/toasts/toasts.store";
-import useReactQueryMutation from "x-hooks/use-react-query-mutation";
+import { useUploadToIPFS } from "x-hooks/use-upload-to-ipfs";
 
 type UploadCommandProps = {
   accept: string;
   onUploaded: (url: string) => void;
 }
-
-const { publicRuntimeConfig } = getConfig();
-const ipfsUrl = publicRuntimeConfig?.urls?.ipfs;
 
 export function UploadCommand({
   accept,
@@ -24,23 +17,20 @@ export function UploadCommand({
 }: UploadCommandProps) {
   const inputRef = useRef<HTMLInputElement>();
 
-  const { addWarning } = useToastStore();
-  const { mutate: uploadFiles, isPending: isUploading } = useReactQueryMutation({
-    mutationFn: useUploadFile,
-    onSuccess: (data) => {
-      const isImage = /\.(gif|jpg|jpeg|tiff|png)$/i.test(data[0].fileName);
-      let url = `[${data[0].fileName}](${ipfsUrl}/${data[0].hash})`;
+  const { mutate: uploadFiles, isPending: isUploading } = useUploadToIPFS({
+    accept,
+    onSuccess: (files) =>  {
+      const uploaded = files.at(0);
+      const isImage = /\.(gif|jpg|jpeg|tiff|png)$/i.test(uploaded.name);
+      let url = `[${uploaded.name}](${uploaded.url})`;
       if (isImage)
         url = "!" + url;
       onUploaded(url);
     },
-    onError: (error) => {
-      console.log(error);
-    }
   });
 
   function onClick() {
-    if (!inputRef.current || !ipfsUrl) return;
+    if (!inputRef.current) return;
     inputRef.current.click();
   }
 
@@ -48,12 +38,6 @@ export function UploadCommand({
     const files = e.target.files;
 
     if (!files.length) return;
-
-    const fileType = files[0].type;
-
-    if (!accept.includes(fileType)) {
-      addWarning('Warning', 'Unsupported file');
-    }
 
     uploadFiles(files[0]);
   }
