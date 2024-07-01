cb5d096183b0a6b75ffd33abf2d3d63b12395751
MoshMage <moshmage@gmail.com>
2024-07-01 09:20:55 +0100

    optional chain on email-notification-subjects

--- server/templates/index.ts
+++ server/templates/index.ts
@@ -9,7 +9,7 @@ export const EmailTemplates = {
   EmailVerification: `${basePath}/emails/email-verification.hbs`,
 }
 
-export const EmailNotificationSubjects: { [k in AnalyticEventName]: string } = {
+export const EmailNotificationSubjects: { [k in AnalyticEventName]?: string } = {
   COMMENT_PROPOSAL: "A comment has been made on your proposal",
   COMMENT_DELIVERABLE: "A comment has been made on your deliverable",
   COMMENT_TASK: "A comment has been made on your task"
