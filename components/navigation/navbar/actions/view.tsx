7a9258d4d0ef18ff8040979c99a4c528051ce68f
moshmage <moshmage@gmail.com>
2024-07-12 11:32:22 +0100

    Bepro 2223 (#472)
    * add points to the navbar if user is connected
    
    * don't need points translations on navbar
    
    * fix imports on navbar view

--- components/navigation/navbar/actions/view.tsx
+++ components/navigation/navbar/actions/view.tsx
@@ -1,25 +1,30 @@
-import { useTranslation } from "next-i18next";
+import {useTranslation} from "next-i18next";
+import NextLink from "next/link";
 
 import HelpButton from "components/common/buttons/help/view";
-import { Tooltip } from "components/common/tooltip/tooltip.view";
+import {PointsBadge} from "components/common/points/points-badge.view";
+import {Tooltip} from "components/common/tooltip/tooltip.view";
 import ConnectWalletButton from "components/connections/connect-wallet-button/connect-wallet-button.controller";
 import CreateNetworkBountyButton from "components/create-network-bounty-button/controller";
+import If from "components/If";
 import NavAvatar from "components/nav-avatar";
 import HamburgerButton from "components/navigation/hamburger/controller";
 import Notifications from "components/notifications/controller";
 import ResponsiveWrapper from "components/responsive-wrapper";
 import TransactionsStateIndicator from "components/transactions-state-indicator";
 
-export default function NavBarActions() {
+export default function NavBarActions({userPoints = "0", connected = false}) {
   const { t } = useTranslation("common");
   
   return(
     <>
       <div className="d-flex flex-row align-items-center gap-3">
+        <If condition={connected}>
+          <NextLink href="/points"><span className="cursor-pointer"><PointsBadge points={userPoints} size="sm" variant="filled"/></span></NextLink>
+        </If>
         <ResponsiveWrapper
           xs={false}
-          md={true}
-        >
+          md={true}>
           <div className="d-flex gap-3 align-items-center">
             <CreateNetworkBountyButton />
 
