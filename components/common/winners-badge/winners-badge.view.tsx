f2099c7071dfb77ede61403c3da058bb5680c64f
vhcsilva <vhcsilvaa@gmail.com>
2024-08-05 13:36:46 -0300

    add tool tip to badge

--- components/common/winners-badge/winners-badge.view.tsx
+++ components/common/winners-badge/winners-badge.view.tsx
@@ -3,16 +3,19 @@ import { useTranslation } from "next-i18next";
 import UserIcon from "assets/icons/user";
 import UsersIcon from "assets/icons/users";
 
+import { Tooltip } from "components/common/tooltip/tooltip.view";
 import { ResponsiveEle } from "components/responsive-wrapper";
 
 interface WinnersBadgeProps {
   isMultiple? : boolean;
   responsiveLabel?: boolean;
+  onlyIcon?: boolean;
 }
 
 export function WinnersBadge({
   isMultiple,
   responsiveLabel,
+  onlyIcon,
 }: WinnersBadgeProps) {
   const { t } = useTranslation("bounty");
 
@@ -20,6 +23,9 @@ export function WinnersBadge({
   const label = isMultiple ? t("multiple-winners") : t("single-winner");
 
   function renderLabel() {
+    if (onlyIcon) 
+      return <></>;
+
     if (responsiveLabel)
       return (
         <ResponsiveEle
@@ -33,10 +39,12 @@ export function WinnersBadge({
   }
 
   return(
-    <div className={`d-flex align-items-center gap-2 text-gray-200 xs-medium bg-gray-850 
-      border border-gray-800 border-radius-4 p-2`}>
-      {icon}
-      {renderLabel()}
-    </div>
+    <Tooltip tip={label}>
+      <div className={`d-flex align-items-center gap-2 text-gray-200 xs-medium bg-gray-850 
+        border border-gray-800 border-radius-4 p-2`}>
+        {icon}
+        {renderLabel()}
+      </div>
+    </Tooltip>
   );
 }
