82a61a49b7a26484dc6a81cf7cdfaaae0f16bf44
vhcsilva <vhcsilvaa@gmail.com>
2024-08-08 13:57:13 -0300

    fix template names

--- server/services/notifications/templates.ts
+++ server/services/notifications/templates.ts
@@ -7,6 +7,6 @@ export const Templates: { [k in AnalyticEventName]?: string } = {
   NOTIF_REPLY_TO_THREAD_CREATOR: "reply-thread-creator.hbs",
   REPLY_TO_THREAD_PARTICIPANT: "reply-thread-participant.hbs",
   NOTIF_REPLY_TO_THREAD_PARTICIPANT: "reply-thread-participant.hbs",
-  SUBSCRIBER_COMMENT: "reply-thread-participant.hbs",
-  NOTIF_SUBSCRIBER_COMMENT: "reply-thread-participant.hbs",
+  SUBSCRIBER_COMMENT: "comment-subscriber.hbs",
+  NOTIF_SUBSCRIBER_COMMENT: "comment-subscriber.hbs",
 }
