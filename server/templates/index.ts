a8094e90852a45b26a4c8d180b80fbace93e27bb
vhcsilva <vhcsilvaa@gmail.com>
2024-08-08 14:34:31 -0300

    add missing email subjects

--- server/templates/index.ts
+++ server/templates/index.ts
@@ -12,5 +12,8 @@ export const EmailTemplates = {
 export const EmailNotificationSubjects: { [k in AnalyticEventName]?: string } = {
   COMMENT_PROPOSAL: "A comment has been made on your proposal",
   COMMENT_DELIVERABLE: "A comment has been made on your deliverable",
-  COMMENT_TASK: "A comment has been made on your task"
+  COMMENT_TASK: "A comment has been made on your task",
+  REPLY_TO_THREAD_CREATOR: "A new reply on your comment",
+  REPLY_TO_THREAD_PARTICIPANT: "A new reply on a thread you participate",
+  SUBSCRIBER_COMMENT: "A new comment on a task you're subscribed"
 }
