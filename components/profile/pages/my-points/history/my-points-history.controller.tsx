694f4a3ec5647d5def75748643d95fb754629bfe
Vitor Hugo <vhcsilvaa@gmail.com>
2024-08-09 09:14:26 -0300

    Bepro 2245 add pagination to history points (#490)
    * fix error translation
    
    * remove unused hook
    
    * add icons
    
    * add pagination to list

--- components/profile/pages/my-points/history/my-points-history.controller.tsx
+++ components/profile/pages/my-points/history/my-points-history.controller.tsx
@@ -1,7 +1,71 @@
-import {userPointsOfUser} from "../../../../../x-hooks/use-points-of-user";
-import {MyPointsHistoryView} from "./my-points-history.view";
+import { useState } from "react";
+
+import { DEFAULT_ITEMS_PER_PAGE, paginateArray } from "helpers/paginate";
+
+import { userPointsOfUser } from "x-hooks/use-points-of-user";
+
+import { MyPointsHistoryView, PageElement } from "./my-points-history.view";
 
 export function MyPointsHistory() {
-  const {collectedPoints, fetchingCollectedPoints} = userPointsOfUser();
-  return <MyPointsHistoryView loading={fetchingCollectedPoints} history={collectedPoints} />
+  const [page, setPage] = useState(1);
+
+  const { collectedPoints, fetchingCollectedPoints } = userPointsOfUser();
+
+  const { data, pages } = paginateArray(collectedPoints, DEFAULT_ITEMS_PER_PAGE, page);
+
+  function getPagesElement() {
+    const separatorElement = { label: "...", isSeparator: true };
+    const pagesToRender = 5;
+    const pagesElements: PageElement[] = Array(pages)
+      .fill(1)
+      .map((e, i) => ({
+        label: e + i,
+        isActive: page === (e + i),
+        onClick: () => setPage(e + i)
+      }));
+    const pagesQuantity = pagesElements.length;
+
+    if (pages <= pagesToRender)
+      return pagesElements;
+
+    if (page < 4)
+      return [...pagesElements.slice(0, 4), separatorElement, pagesElements[pagesQuantity - 1]];
+    else if (page > pagesQuantity - 3)
+      return [pagesElements[0], separatorElement, ...pagesElements.slice(pagesQuantity - 4, pagesQuantity)];
+    else
+      return [
+        pagesElements[0], 
+        separatorElement,
+        pagesElements[page - 2],
+        pagesElements[page - 1],
+        pagesElements[page],
+        separatorElement,
+        pagesElements[pagesQuantity - 1],
+      ];
+  }
+
+  function onPreviousClick() {
+    if (page === 1)
+      return;
+
+    setPage(previous => previous -1);
+  }
+
+  function onNextClick() {
+    if (page === pages)
+      return;
+
+    setPage(previous => previous + 1);
+  }
+
+  return (
+    <MyPointsHistoryView 
+      loading={fetchingCollectedPoints} 
+      history={data}
+      page={page}
+      pages={getPagesElement()}
+      onPreviousClick={onPreviousClick}
+      onNextClick={onNextClick}
+    />
+  );
 }
